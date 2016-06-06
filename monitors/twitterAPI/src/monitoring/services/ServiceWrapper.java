cba404980415a936785d83d5bb72f9659e853a2b
jmotger <joaquim.motger@est.fib.upc.edu>
2016-06-06 13:58:49 +0200

    first version of retrieving twitter monitor data

--- monitors/twitterAPI/src/monitoring/services/ServiceWrapper.java
+++ monitors/twitterAPI/src/monitoring/services/ServiceWrapper.java
@@ -13,22 +13,28 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import monitoring.params.MonitoringData;
 import monitoring.params.MonitoringParams;
 
 public abstract class ServiceWrapper {
 	
+	private int responseDataId = 1;
+	private int responseId = 1;
+	
 	public abstract String addConfiguration(String jsonConf);
 	
-	public JSONObject getResponse(int id, String status) {
+	public JSONObject getResponse(String status) {
 		
 		JSONObject response = new JSONObject();
 		JSONObject resInfo = new JSONObject();
 		
 		try {
 			
-			resInfo.put("idConf", id);
+			resInfo.put("idConf", responseId);
 			resInfo.put("status", status);
 			response.put("SocialNetworksMonitoringConfProfResult", resInfo);
+			
+			++responseId;
 			
 			return response;
 			
@@ -40,6 +46,49 @@ public abstract class ServiceWrapper {
 		
 	}
 	
+	public void getData(String timeStamp, List<MonitoringData> data) {
+		
+		try {
+			JSONArray items = new JSONArray();
+			for (MonitoringData item : data) {
+				JSONObject jsonItem = new JSONObject();
+				
+				jsonItem.put("idItem", item.getId());
+				jsonItem.put("timeStamp", item.getTimeStamp());
+				jsonItem.put("message", item.getMessage());
+				jsonItem.put("author", item.getAuthor());
+				jsonItem.put("link", item.getLink());
+				
+				items.put(jsonItem);
+			}
+			
+			JSONObject mainInfo = new JSONObject();
+			
+			mainInfo.put("idOutput", responseDataId);
+			mainInfo.put("searchTimeStamp", timeStamp);
+			mainInfo.put("numDataItems", data.size());
+			mainInfo.put("DataItems", items);
+			
+			++responseDataId;
+			
+			JSONObject jsonData = new JSONObject();
+			
+			jsonData.put("socialNetworksMonitoredData", mainInfo);
+			
+			sendToKafka(jsonData);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void sendToKafka(JSONObject data) {
+		
+		System.out.println(data);
+		
+	}
+
 	public MonitoringParams parseConfigurationParams(String json) throws JSONException {
 		
 		JSONObject jsonParams = new JSONObject(json);
@@ -51,15 +100,15 @@ public abstract class ServiceWrapper {
 		while( keys.hasNext() ) {
 			
 		    String key = (String)keys.next();
-		    if (key.equals("timeSlot")) params.setTimeSlot(jsonParams.getString(key));
-		    else if (key.equals("kafkaEndpoint")) params.setKafkaEndpoint(jsonParams.getString(key));
-		    else if (key.equals("kafkaTopic")) params.setKafkaTopic(jsonParams.getString(key));
-		    else if (key.equals("keywordExpression")) params.setKeywordExpression(jsonParams.getString(key));
+		    if (key.equals("timeSlot")) params.setTimeSlot(jsonParams.getString(key).replaceAll("\"", ""));
+		    else if (key.equals("kafkaEndpoint")) params.setKafkaEndpoint(jsonParams.getString(key).replaceAll("\"", ""));
+		    else if (key.equals("kafkaTopic")) params.setKafkaTopic(jsonParams.getString(key).replaceAll("\"", ""));
+		    else if (key.equals("keywordExpression")) params.setKeywordExpression(jsonParams.getString(key).replaceAll("\"", ""));
 		    else if (key.equals("accounts")) {
 		    	JSONArray jsonAccounts = jsonParams.getJSONArray(key);
 	    		List<String> accounts = new ArrayList<>();
 	    		for (int i = 0; i < jsonAccounts.length(); ++i) {
-	    			accounts.add(jsonAccounts.getString(i));
+	    			accounts.add(jsonAccounts.getString(i).replaceAll("\"", "").replaceAll("@", ""));
 	    		}
 	    		params.setAccounts(accounts);
 		    }
@@ -68,5 +117,7 @@ public abstract class ServiceWrapper {
 		return params;
 		
 	}
+	
+	
 
 }
--- monitors/twitterAPI/src/monitoring/services/SocialNetworkService.java
+++ monitors/twitterAPI/src/monitoring/services/SocialNetworkService.java
@@ -1,21 +1,25 @@
 package monitoring.services;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
+import monitoring.params.MonitoringData;
 import monitoring.params.MonitoringParams;
+import twitter4j.ConnectionLifeCycleListener;
 import twitter4j.FilterQuery;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
-import twitter4j.User;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 import twitter4j.util.function.Consumer;
@@ -25,6 +29,9 @@ public class SocialNetworkService extends ServiceWrapper {
 	
 	MonitoringParams params;
 	TwitterStream stream;
+	
+	List<Status> tweetInfo;
+	boolean firstConnection;
 
 	@Override
 	@POST
@@ -32,6 +39,8 @@ public class SocialNetworkService extends ServiceWrapper {
 	public String addConfiguration(@QueryParam("configurationJson") String jsonConf) {
 		
 		int id = 1;
+		firstConnection = true;
+		tweetInfo = new ArrayList<>();
 		
 		try {
 			
@@ -51,37 +60,142 @@ public class SocialNetworkService extends ServiceWrapper {
 			stream.onStatus(new Consumer<Status>() {
 				@Override
 				public void accept(Status arg0) {
-					User u = arg0.getUser();
-					boolean userCheck = false;
-					for (int i = 0; i < params.getAccounts().size(); ++i) {
-						System.out.println(u.getScreenName() + "//" + params.getAccounts().get(i));
-						if (u.getScreenName().equals(params.getAccounts().get(i))) userCheck = true;
+					System.out.println("@" + arg0.getUser().getScreenName());
+					if (params.getAccounts() != null && !params.getAccounts().isEmpty()) {
+						if (params.getAccounts().contains(arg0.getUser().getScreenName())); 
+							tweetInfo.add(arg0);
 					}
-					if (userCheck) System.out.println(u.getName()+ "(@"+u.getScreenName()+"): "+arg0.getText());		
+					else tweetInfo.add(arg0);
 				}
+			});
+			
+			stream.addConnectionLifeCycleListener(new ConnectionLifeCycleListener() {
+
+				@Override
+				public void onCleanUp() {
+					
+				}
+
+				@Override
+				public void onConnect() {
+					Timer timer = new Timer();
+					timer.schedule(new TimerTask() {
+					    public void run() {
+					    	if (firstConnection) {
+					    		firstConnection = false;
+					    		System.out.println("First connection stablished");
+					    	} else {
+					    		generateData((new Timestamp((new Date()).getTime()).toString()));
+					    	}
+					    }
+
+					}, 0, Integer.parseInt(params.getTimeSlot())*1000);
+				}
+
+				@Override
+				public void onDisconnect() {
+					System.out.println("Connection closed");
+				}
+				
 			});
 						
 			
 			FilterQuery filterQuery = new FilterQuery();
-			filterQuery.track(params.getKeywordExpression());
+			if (params.getKeywordExpression() != null) {
+				filterQuery.track(generateKeywordExp(params.getKeywordExpression()));
+			}
+			
 			System.out.println(filterQuery);
 			
-			Timer timer = new Timer();
-			timer.schedule(new TimerTask() {
-			    public void run() {
-			    	System.out.println("Ending...");
-			    }
-			}, 0, Integer.parseInt(params.getTimeSlot())*1000);
-			
 			//filterQuery.follow(user.getId());
-			stream.filter(params.getKeywordExpression());
+			stream.filter(filterQuery);
 
 		} catch (Exception e1) {
 			e1.printStackTrace();
-			return getResponse(id, "error").toString();
+			return getResponse("error").toString();
 		}
 
-		return getResponse(id, "success").toString();
+		return getResponse("success").toString();
+		
+	}
+
+	private String[] generateKeywordExp(String keywordExpression) {
+		
+		String[] blocks = keywordExpression.split(" AND ");
+		for (int i = 0; i < blocks.length; ++i) {
+			blocks[i] = blocks[i].replace("(", "");
+			blocks[i] = blocks[i].replace(")", "");
+		}
+		
+		List<List<String>> cnfCombinations = new ArrayList<>();
+		
+		for (int i = 0; i < blocks.length; ++i) {
+			cnfCombinations.add(getKeyElements(blocks[i]));
+		}
+		
+		List<List<String>> dnfCombinations = new ArrayList<>();
+		
+		for (int i = 0; i < cnfCombinations.get(0).size(); ++i) {
+			dnfCombinations.addAll(getDnfCombination(cnfCombinations, 0, i));
+		}
+		
+		String[] keywordDNFExpression = new String[dnfCombinations.size()];
+		
+		for (int i = 0; i < dnfCombinations.size(); ++i) {
+			String keyword = "";
+			for (String s : dnfCombinations.get(i)) {
+				keyword += s + " ";
+			}
+			keywordDNFExpression[i] = keyword;
+		}
+		
+		return keywordDNFExpression;
+		
+	}
+	
+	/**
+	 * Backtracking method for getting all combinations in DNF
+	 */
+	private List<List<String>> getDnfCombination(List<List<String>> cnfCombination, int k, int j) {
+		List<List<String>> dnf = new ArrayList<>();
+		if (k == cnfCombination.size() -1 ) {
+			List<String> l = new ArrayList<>();
+			l.add(cnfCombination.get(k).get(j));
+			dnf.add(l);
+		}
+		else {
+			for (int i = 0; i < cnfCombination.get(k+1).size(); ++i) {
+				List<List<String>> lists = getDnfCombination(cnfCombination, k + 1, i);
+				for (List<String> l : lists) l.add(cnfCombination.get(k).get(j));
+				dnf.addAll(lists);
+			}
+		}
+		return dnf;
+	}
+	
+	private List<String> getKeyElements(String block) {
+		String[] elements = block.split(" OR ");
+		List<String> keyElements = new ArrayList<>();
+		for (int i = 0; i < elements.length; ++i) {
+			keyElements.add(elements[i]);
+		}
+		return keyElements;
+	}
+
+	private void generateData(String searchTimeStamp) {
+		
+		List<MonitoringData> data = new ArrayList<>();
+		for (Status s : tweetInfo) {
+			String id = String.valueOf(s.getId());
+			String timeStamp = String.valueOf(s.getCreatedAt());
+			String message = s.getText();
+			String author = "@" + s.getUser().getScreenName();
+			String link = "https://twitter.com/" + s.getUser().getName().replace(" ", "")+ "/status/" + s.getId();
+			MonitoringData dataObj = new MonitoringData(id, timeStamp, message, author, link);
+			data.add(dataObj);
+		}
+		tweetInfo = new ArrayList<>();
+		getData(searchTimeStamp, data);
 		
 	}
 	
