0f3ac5806bf47bbc2d01fb281b9c957eb2acfc48
jmotger <joaquim.motger@est.fib.upc.edu>
2016-06-09 14:37:05 +0200

    fixed user filtering bug

--- monitors/twitterAPI/src/monitoring/services/SocialNetworkService.java
+++ monitors/twitterAPI/src/monitoring/services/SocialNetworkService.java
@@ -63,8 +63,9 @@ public class SocialNetworkService extends ServiceWrapper {
 				@Override
 				public void accept(Status arg0) {
 					if (params.getAccounts() != null && !params.getAccounts().isEmpty()) {
-						if (params.getAccounts().contains(arg0.getUser().getScreenName())); 
+						if (params.getAccounts().contains(arg0.getUser().getScreenName())) { 
 							tweetInfo.add(arg0);
+						}
 					}
 					else tweetInfo.add(arg0);
 				}
