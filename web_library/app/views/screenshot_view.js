25cb5f81346888131338df8528e821d3d10cabd1
rschaniel <ronnieschaniel@gmail.com>
2016-07-04 15:13:00 +0200

    added annotation functionality to screenshot

--- web_library/app/views/screenshot_view.js
+++ web_library/app/views/screenshot_view.js
@@ -1,35 +1,40 @@
 define(["require", "exports", '../js/helper', '../js/lib/html2canvas.js'], function (require, exports, helper_1) {
     "use strict";
+    var myThis;
     var ScreenshotView = (function () {
         function ScreenshotView(screenshotMechanism, screenshotPreviewElement, screenshotCaptureButton, elementToCapture, elementsToHide) {
+            myThis = this;
             this.screenshotMechanism = screenshotMechanism;
             this.screenshotPreviewElement = screenshotPreviewElement;
             this.screenshotCaptureButton = screenshotCaptureButton;
             this.elementToCapture = elementToCapture;
             this.elementsToHide = elementsToHide;
+            this.canvasState = null;
             this.addCaptureEventToButton();
         }
         ScreenshotView.prototype.generateScreenshot = function () {
-            $('.ui-widget-overlay.ui-front').hide();
-            $('.ui-dialog').hide();
-            var screenshotViewObject = this;
+            this.hideElements();
             html2canvas(this.elementToCapture, {
                 onrendered: function (canvas) {
-                    $('.ui-widget-overlay.ui-front').show();
-                    $('.ui-dialog').show();
-                    screenshotViewObject.screenshotPreviewElement.empty().append(canvas);
-                    screenshotViewObject.screenshotPreviewElement.show();
-                    var windowRatio = screenshotViewObject.elementToCapture.width() / screenshotViewObject.elementToCapture.height();
+                    myThis.showElements();
+                    myThis.screenshotPreviewElement.empty().append(canvas);
+                    myThis.screenshotPreviewElement.show();
+                    var windowRatio = myThis.elementToCapture.width() / myThis.elementToCapture.height();
                     var data = canvas.toDataURL();
-                    var context = canvas.getContext("2d");
-                    $(canvas).prop('width', screenshotViewObject.screenshotPreviewElement.width());
-                    $(canvas).prop('height', screenshotViewObject.screenshotPreviewElement.width() / windowRatio);
+                    myThis.context = canvas.getContext("2d");
+                    myThis.canvasWidth = myThis.screenshotPreviewElement.width();
+                    myThis.canvasHeight = myThis.screenshotPreviewElement.width() / windowRatio;
+                    $(canvas).prop('width', myThis.canvasWidth);
+                    $(canvas).prop('height', myThis.canvasHeight);
                     var img = new Image();
                     img.onload = function () {
-                        context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+                        myThis.context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
                     };
                     img.src = data;
-                    screenshotViewObject.screenshotCanvas = canvas;
+                    myThis.canvasState = new Image();
+                    myThis.canvasState.src = img.src;
+                    myThis.screenshotCanvas = canvas;
+                    myThis.initDrawing();
                 }
             });
         };
@@ -38,11 +43,68 @@ define(["require", "exports", '../js/helper', '../js/lib/html2canvas.js'], funct
             return helper_1.Helper.dataURItoBlob(dataURL);
         };
         ScreenshotView.prototype.addCaptureEventToButton = function () {
-            var screenshotViewObject = this;
+            var myThis = this;
             this.screenshotCaptureButton.on('click', function (event) {
                 event.preventDefault();
                 event.stopPropagation();
-                screenshotViewObject.generateScreenshot();
+                myThis.generateScreenshot();
+            });
+        };
+        ScreenshotView.prototype.hideElements = function () {
+            for (var _i = 0, _a = this.elementsToHide; _i < _a.length; _i++) {
+                var elementToHide = _a[_i];
+                elementToHide.hide();
+            }
+        };
+        ScreenshotView.prototype.showElements = function () {
+            for (var _i = 0, _a = this.elementsToHide; _i < _a.length; _i++) {
+                var elementToHide = _a[_i];
+                elementToHide.show();
+            }
+        };
+        ScreenshotView.prototype.initDrawing = function () {
+            var context = this.screenshotCanvas.getContext('2d');
+            this.drawingMode = 'fillRect';
+            this.isPainting = false;
+            $(this.screenshotCanvas).on('mousedown touchstart', function (event) {
+                var parentOffset = $(this).parent().offset();
+                myThis.startX = event.pageX - parentOffset.left;
+                myThis.startY = event.pageY - parentOffset.top;
+                myThis.isPainting = true;
+            }).on('mousemove touchmove', function (event) {
+                if (myThis.isPainting) {
+                    context.clearRect(0, 0, myThis.canvasWidth, myThis.canvasHeight);
+                    context.drawImage(myThis.canvasState, 0, 0, myThis.canvasState.width, myThis.canvasState.height, 0, 0, myThis.screenshotCanvas.width, myThis.screenshotCanvas.height);
+                    var parentOffset = $(this).parent().offset();
+                    var currentX = event.pageX - parentOffset.left;
+                    var currentY = event.pageY - parentOffset.top;
+                    var width = currentX - myThis.startX;
+                    var height = currentY - myThis.startY;
+                    if (myThis.drawingMode === 'rect') {
+                        context.rect(myThis.startX, myThis.startY, width, height);
+                    }
+                    else if (myThis.drawingMode === 'fillRect') {
+                        context.fillRect(myThis.startX, myThis.startY, width, height);
+                    }
+                    context.stroke();
+                }
+            }).on('mouseup touchend', function (event) {
+                myThis.isPainting = false;
+                var parentOffset = $(this).parent().offset();
+                var endX = event.pageX - parentOffset.left;
+                var endY = event.pageY - parentOffset.top;
+                var width = endX - myThis.startX;
+                var height = endY - myThis.startY;
+                if (myThis.drawingMode === 'rect') {
+                    context.rect(myThis.startX, myThis.startY, width, height);
+                }
+                else if (myThis.drawingMode === 'fillRect') {
+                    context.fillRect(myThis.startX, myThis.startY, width, height);
+                }
+                context.stroke();
+                myThis.canvasState.src = myThis.screenshotCanvas.toDataURL("image/jpeg");
+            }).on('mouseleave touchleave', function () {
+                myThis.isPainting = false;
             });
         };
         return ScreenshotView;
--- web_library/app/views/screenshot_view.ts
+++ web_library/app/views/screenshot_view.ts
@@ -2,6 +2,8 @@ import {Mechanism} from '../models/mechanism';
 import '../js/lib/html2canvas.js';
 import {Helper} from '../js/helper';
 
+var myThis;
+
 
 export class ScreenshotView {
     screenshotMechanism:Mechanism;
@@ -10,45 +12,57 @@ export class ScreenshotView {
     elementToCapture:JQuery;
     elementsToHide:[JQuery];
     screenshotCanvas:any;
+    context:any;
+    startX:number;
+    startY:number;
+    drawingMode:string;
+    canvasState:HTMLImageElement;
+    isPainting:boolean;
+    canvasWidth:number;
+    canvasHeight:number;
 
     constructor(screenshotMechanism:Mechanism, screenshotPreviewElement:JQuery, screenshotCaptureButton:JQuery,
                 elementToCapture:JQuery, elementsToHide:any) {
+        myThis = this;
         this.screenshotMechanism = screenshotMechanism;
         this.screenshotPreviewElement = screenshotPreviewElement;
         this.screenshotCaptureButton = screenshotCaptureButton;
         this.elementToCapture = elementToCapture;
         this.elementsToHide = elementsToHide;
+        this.canvasState = null;
         this.addCaptureEventToButton();
     }
 
     generateScreenshot() {
-        $('.ui-widget-overlay.ui-front').hide();
-        $('.ui-dialog').hide();
-
-        var screenshotViewObject = this;
+        this.hideElements();
 
         html2canvas(this.elementToCapture, {
             onrendered: function (canvas) {
-                $('.ui-widget-overlay.ui-front').show();
-                $('.ui-dialog').show();
+                myThis.showElements();
+                myThis.screenshotPreviewElement.empty().append(canvas);
+                myThis.screenshotPreviewElement.show();
 
-                screenshotViewObject.screenshotPreviewElement.empty().append(canvas);
-                screenshotViewObject.screenshotPreviewElement.show();
-
-                var windowRatio = screenshotViewObject.elementToCapture.width() / screenshotViewObject.elementToCapture.height();
+                var windowRatio = myThis.elementToCapture.width() / myThis.elementToCapture.height();
 
                 // save the canvas content as imageURL
                 var data = canvas.toDataURL();
-                var context = canvas.getContext("2d");
-                $(canvas).prop('width', screenshotViewObject.screenshotPreviewElement.width());
-                $(canvas).prop('height', screenshotViewObject.screenshotPreviewElement.width() / windowRatio);
+                myThis.context = canvas.getContext("2d");
+                myThis.canvasWidth = myThis.screenshotPreviewElement.width();
+                myThis.canvasHeight = myThis.screenshotPreviewElement.width() / windowRatio;
+
+                $(canvas).prop('width', myThis.canvasWidth);
+                $(canvas).prop('height', myThis.canvasHeight);
 
                 var img = new Image();
                 img.onload = function () {
-                    context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+                    myThis.context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
                 };
+
                 img.src = data;
-                screenshotViewObject.screenshotCanvas = canvas;
+                myThis.canvasState = new Image();
+                myThis.canvasState.src = img.src;
+                myThis.screenshotCanvas = canvas;
+                myThis.initDrawing();
             }
         });
     }
@@ -59,11 +73,98 @@ export class ScreenshotView {
     }
 
     addCaptureEventToButton() {
-        var screenshotViewObject = this;
+        var myThis = this;
         this.screenshotCaptureButton.on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
-            screenshotViewObject.generateScreenshot();
+            myThis.generateScreenshot();
+        });
+    }
+
+    hideElements() {
+        for(var elementToHide of this.elementsToHide) {
+            elementToHide.hide();
+        }
+    }
+
+    showElements() {
+        for(var elementToHide of this.elementsToHide) {
+            elementToHide.show();
+        }
+    }
+
+    initDrawing() {
+        var context = this.screenshotCanvas.getContext('2d');
+        this.drawingMode = 'fillRect';
+        this.isPainting = false;
+
+        $(this.screenshotCanvas).on('mousedown touchstart', function(event) {
+            var parentOffset = $(this).parent().offset();
+            myThis.startX = event.pageX - parentOffset.left;
+            myThis.startY = event.pageY - parentOffset.top;
+            myThis.isPainting = true;
+        }).on('mousemove touchmove', function(event) {
+            if(myThis.isPainting) {
+                // reset canvas to the saved state to be able to provide a preview
+                context.clearRect(0, 0, myThis.canvasWidth, myThis.canvasHeight);
+                context.drawImage(myThis.canvasState, 0, 0, myThis.canvasState.width,
+                    myThis.canvasState.height, 0, 0, myThis.screenshotCanvas.width,
+                    myThis.screenshotCanvas.height);
+
+                var parentOffset = $(this).parent().offset();
+                var currentX = event.pageX - parentOffset.left;
+                var currentY = event.pageY - parentOffset.top;
+
+                var width = currentX - myThis.startX;
+                var height = currentY - myThis.startY;
+
+                if(myThis.drawingMode === 'rect') {
+                    context.rect(myThis.startX, myThis.startY, width, height);
+                } else if (myThis.drawingMode === 'fillRect') {
+                    context.fillRect(myThis.startX, myThis.startY, width, height);
+                }
+                context.stroke();
+            }
+        }).on('mouseup touchend', function(event) {
+            myThis.isPainting = false;
+
+            var parentOffset = $(this).parent().offset();
+            var endX = event.pageX - parentOffset.left;
+            var endY = event.pageY - parentOffset.top;
+
+            var width = endX - myThis.startX;
+            var height = endY - myThis.startY;
+
+            if(myThis.drawingMode === 'rect') {
+                context.rect(myThis.startX, myThis.startY, width, height);
+            } else if (myThis.drawingMode === 'fillRect') {
+                context.fillRect(myThis.startX, myThis.startY, width, height);
+            }
+            context.stroke();
+
+            // update canvas state
+            myThis.canvasState.src = myThis.screenshotCanvas.toDataURL("image/jpeg");
+        }).on('mouseleave touchleave', function () {
+            myThis.isPainting = false;
         });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
