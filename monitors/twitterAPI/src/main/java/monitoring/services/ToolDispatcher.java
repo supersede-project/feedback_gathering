984a8c30477758155012731ec6497ec970721d53
jmotger <joaquim.motger@est.fib.upc.edu>
2016-07-08 19:27:56 +0200

    service configuration modified

--- monitors/twitterAPI/src/main/java/monitoring/services/ToolDispatcher.java
+++ monitors/twitterAPI/src/main/java/monitoring/services/ToolDispatcher.java
@@ -15,7 +15,7 @@ import org.json.JSONObject;
 import monitoring.kafka.KafkaCommunication;
 import monitoring.model.MonitoringParams;
 
-@Path("SocialNetworkMonitoring")
+@Path("service")
 public class ToolDispatcher {
 	
 	//The id of the response associated to a addConfiguration call
