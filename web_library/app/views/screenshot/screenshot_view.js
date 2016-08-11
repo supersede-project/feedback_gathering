5ef5d6c05c2e51512f7bb71954187e3b4d1e5d82
rschaniel <ronnieschaniel@gmail.com>
2016-08-11 15:10:49 +0200

    added text color change to text field on screenshot

--- web_library/app/views/screenshot/screenshot_view.js
+++ web_library/app/views/screenshot/screenshot_view.js
@@ -343,9 +343,36 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
                 var stickerContainer = jQuery('<div class="sticker-container text-2">' +
                     '<textarea placeholder="Your text"></textarea>' +
                     '<a class="remove"><img src="dist/img/ic_remove_circle_red_shadow_24px.png" /></a>' +
+                    '<a class="color" style="background-image: url(\'dist/img/screenshot_button_background.png\');"><i class="material-icons">format_color_text</i></a>' +
                     '</div>');
+                var textarea = stickerContainer.find('textarea');
+                var colorLink = stickerContainer.find('a.color');
+                var defaultColor = "#000";
+                textarea.css('color', defaultColor);
                 stickerContainer.css('width', 'auto');
                 stickerContainer.css('height', 'auto');
+                stickerContainer.find('a.color').spectrum({
+                    color: defaultColor,
+                    showPaletteOnly: true,
+                    togglePaletteOnly: true,
+                    togglePaletteMoreText: 'more',
+                    togglePaletteLessText: 'less',
+                    palette: [
+                        ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
+                        ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
+                        ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
+                        ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
+                        ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
+                        ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
+                        ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
+                        ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
+                    ],
+                    change: function (color) {
+                        var color = color.toHexString();
+                        textarea.css('color', color);
+                        colorLink.css('color', color);
+                    }
+                });
                 var containmentSelector = '#' + myThis.container.attr('id') + ' .screenshot-preview';
                 stickerContainer.resizable({
                     containment: containmentSelector,
--- web_library/app/views/screenshot/screenshot_view.ts
+++ web_library/app/views/screenshot/screenshot_view.ts
@@ -53,7 +53,7 @@ export class ScreenshotView {
     }
 
     checkAutoTake() {
-        if(this.screenshotMechanism.getParameterValue('autoTake')) {
+        if (this.screenshotMechanism.getParameterValue('autoTake')) {
             this.generateScreenshot();
         }
     }
@@ -96,7 +96,7 @@ export class ScreenshotView {
     }
 
     getScreenshotAsBinary() {
-        if(this.screenshotCanvas) {
+        if (this.screenshotCanvas) {
             var dataURL = this.screenshotCanvas.toDataURL("image/png");
             return DataHelper.dataURItoBlob(dataURL);
         } else {
@@ -114,16 +114,16 @@ export class ScreenshotView {
     }
 
     hideElements() {
-        if(this.elementsToHide != null) {
-            for(var elementToHide of this.elementsToHide) {
+        if (this.elementsToHide != null) {
+            for (var elementToHide of this.elementsToHide) {
                 elementToHide.hide();
             }
         }
     }
 
     showElements() {
-        if(this.elementsToHide != null) {
-            for(var elementToHide of this.elementsToHide) {
+        if (this.elementsToHide != null) {
+            for (var elementToHide of this.elementsToHide) {
                 elementToHide.show();
             }
         }
@@ -137,18 +137,18 @@ export class ScreenshotView {
         this.context.strokeStyle = red;
         var myThis = this;
 
-        jQuery(this.screenshotCanvas).on('mousedown touchstart', function(event) {
+        jQuery(this.screenshotCanvas).on('mousedown touchstart', function (event) {
             var parentOffset = jQuery(this).parent().offset();
             myThis.startX = event.pageX - parentOffset.left;
             myThis.startY = event.pageY - parentOffset.top;
 
-            if(myThis.drawingMode === freehandDrawingMode) {
+            if (myThis.drawingMode === freehandDrawingMode) {
                 context.beginPath();
                 context.moveTo(myThis.startX, myThis.startY);
             }
             myThis.isPainting = true;
-        }).on('mousemove touchmove', function(event) {
-            if(myThis.isPainting) {
+        }).on('mousemove touchmove', function (event) {
+            if (myThis.isPainting) {
                 // reset canvas to the saved state to be able to provide a preview
                 context.clearRect(0, 0, myThis.canvasWidth, myThis.canvasHeight);
                 context.drawImage(myThis.canvasState, 0, 0, myThis.canvasState.width,
@@ -161,7 +161,7 @@ export class ScreenshotView {
                 var width = currentX - myThis.startX;
                 var height = currentY - myThis.startY;
 
-                if(myThis.drawingMode === rectDrawingMode) {
+                if (myThis.drawingMode === rectDrawingMode) {
                     // TODO extract all the drawing to various drawers that implement drawstart and drawend and implement the same interface
                     context.beginPath();
                     context.strokeRect(myThis.startX, myThis.startY, width, height);
@@ -171,7 +171,7 @@ export class ScreenshotView {
                 } else if (myThis.drawingMode === circleDrawingMode) {
                     context.beginPath();
                     var radius = height > width ? height : width;
-                    context.arc(myThis.startX, myThis.startY, radius, 0, Math.PI*2);
+                    context.arc(myThis.startX, myThis.startY, radius, 0, Math.PI * 2);
                 } else if (myThis.drawingMode === freehandDrawingMode) {
                     context.lineTo(currentX, currentY);
                 } else if (myThis.drawingMode === arrowDrawingMode) {
@@ -183,7 +183,7 @@ export class ScreenshotView {
                 }
                 context.stroke();
             }
-        }).on('mouseup touchend', function(event) {
+        }).on('mouseup touchend', function (event) {
             myThis.isPainting = false;
 
             var parentOffset = jQuery(this).parent().offset();
@@ -192,13 +192,13 @@ export class ScreenshotView {
             var width = endX - myThis.startX;
             var height = endY - myThis.startY;
 
-            if(myThis.drawingMode === rectDrawingMode) {
+            if (myThis.drawingMode === rectDrawingMode) {
                 context.strokeRect(myThis.startX, myThis.startY, width, height);
             } else if (myThis.drawingMode === fillRectDrawingMode) {
                 context.fillRect(myThis.startX, myThis.startY, width, height);
             } else if (myThis.drawingMode === circleDrawingMode) {
                 var radius = height > width ? height : width;
-                context.arc(myThis.startX, myThis.startY, radius, 0, Math.PI*2);
+                context.arc(myThis.startX, myThis.startY, radius, 0, Math.PI * 2);
             } else if (myThis.drawingMode === freehandDrawingMode) {
                 context.lineTo(endX, endY);
             } else if (myThis.drawingMode === arrowDrawingMode) {
@@ -217,8 +217,8 @@ export class ScreenshotView {
 
 
                 /* if original image is the current canvas image we have to adjust some values, since the original
-                    width and height differ from the width and height set after an annotation and redrawing and the
-                    crop would be to small and not at the right position  */
+                 width and height differ from the width and height set after an annotation and redrawing and the
+                 crop would be to small and not at the right position  */
                 if (myThis.canvasStates === null || myThis.canvasStates.length === 0) {
                     width = myThis.canvasOriginalWidth / myThis.canvasWidth * width;
                     height = myThis.canvasOriginalHeight / myThis.canvasHeight * height;
@@ -244,7 +244,7 @@ export class ScreenshotView {
 
     reset() {
         this.screenshotPreviewElement.hide();
-        if(this.context) {
+        if (this.context) {
             this.context.clearRect(0, 0, this.context.width, this.context.height);
         }
         this.screenshotCanvas = null;
@@ -258,23 +258,23 @@ export class ScreenshotView {
 
     initRectAsDefaultDrawing() {
         var myThis = this;
-        setTimeout(function() {
+        setTimeout(function () {
             myThis.container.find('.screenshot-draw-rect').addClass('active', 500);
             myThis.drawingMode = rectDrawingMode;
         }, 1500);
     }
 
-    draw_arrow(context, fromx, fromy, tox, toy){
-        if(Math.sqrt(Math.pow(fromx - tox, 2) + Math.pow(fromy - toy, 2)) < 20) {
+    draw_arrow(context, fromx, fromy, tox, toy) {
+        if (Math.sqrt(Math.pow(fromx - tox, 2) + Math.pow(fromy - toy, 2)) < 20) {
             return;
         }
         var headLength = 10;
-        var angle = Math.atan2(toy-fromy,tox-fromx);
+        var angle = Math.atan2(toy - fromy, tox - fromx);
         context.moveTo(fromx, fromy);
         context.lineTo(tox, toy);
-        context.lineTo(tox-headLength*Math.cos(angle-Math.PI/6),toy-headLength*Math.sin(angle-Math.PI/6));
+        context.lineTo(tox - headLength * Math.cos(angle - Math.PI / 6), toy - headLength * Math.sin(angle - Math.PI / 6));
         context.moveTo(tox, toy);
-        context.lineTo(tox-headLength*Math.cos(angle+Math.PI/6),toy-headLength*Math.sin(angle+Math.PI/6));
+        context.lineTo(tox - headLength * Math.cos(angle + Math.PI / 6), toy - headLength * Math.sin(angle + Math.PI / 6));
     }
 
     updateCanvasState() {
@@ -283,7 +283,7 @@ export class ScreenshotView {
     }
 
     undoOperation() {
-        if(this.canvasStates.length < 1) {
+        if (this.canvasStates.length < 1) {
             return;
         }
 
@@ -297,7 +297,7 @@ export class ScreenshotView {
 
     initScreenshotOperations() {
         var myThis = this;
-        this.container.find('.screenshot-draw-rect').on('click', function(event) {
+        this.container.find('.screenshot-draw-rect').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -306,10 +306,10 @@ export class ScreenshotView {
             myThis.drawingMode = rectDrawingMode;
             myThis.context.strokeStyle = red;
             myThis.context.fillStyle = red;
-            myThis.context.setLineDash([0,0]);
+            myThis.context.setLineDash([0, 0]);
         });
 
-        this.container.find('.screenshot-draw-fill-rect').on('click', function(event) {
+        this.container.find('.screenshot-draw-fill-rect').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -318,10 +318,10 @@ export class ScreenshotView {
             myThis.drawingMode = fillRectDrawingMode;
             myThis.context.strokeStyle = black;
             myThis.context.fillStyle = black;
-            myThis.context.setLineDash([0,0]);
+            myThis.context.setLineDash([0, 0]);
         });
 
-        this.container.find('.screenshot-draw-circle').on('click', function(event) {
+        this.container.find('.screenshot-draw-circle').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -330,10 +330,10 @@ export class ScreenshotView {
             myThis.drawingMode = circleDrawingMode;
             myThis.context.strokeStyle = red;
             myThis.context.fillStyle = red;
-            myThis.context.setLineDash([0,0]);
+            myThis.context.setLineDash([0, 0]);
         });
 
-        this.container.find('.screenshot-draw-arrow').on('click', function(event) {
+        this.container.find('.screenshot-draw-arrow').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -342,10 +342,10 @@ export class ScreenshotView {
             myThis.drawingMode = arrowDrawingMode;
             myThis.context.stokeStyle = red;
             myThis.context.fillStyle = red;
-            myThis.context.setLineDash([0,0]);
+            myThis.context.setLineDash([0, 0]);
         });
 
-        this.container.find('.screenshot-draw-freehand').on('click', function(event) {
+        this.container.find('.screenshot-draw-freehand').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -354,10 +354,10 @@ export class ScreenshotView {
             myThis.drawingMode = freehandDrawingMode;
             myThis.context.strokeStyle = red;
             myThis.context.fillStyle = red;
-            myThis.context.setLineDash([0,0]);
+            myThis.context.setLineDash([0, 0]);
         });
 
-        this.container.find('.screenshot-crop').on('click', function(event) {
+        this.container.find('.screenshot-crop').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -369,7 +369,7 @@ export class ScreenshotView {
             myThis.context.setLineDash([3, 8]);
         });
 
-        this.container.find('.screenshot-text').off('click').on('click', function(event) {
+        this.container.find('.screenshot-text').off('click').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -382,7 +382,7 @@ export class ScreenshotView {
                 '<a class="edit"><img src="dist/img/ic_mode_edit_black_shadow_24px.png" /></a>' +
                 '<a class="remove"><img src="dist/img/ic_remove_circle_red_shadow_24px.png" /></a>' +
                 '<article class="text-container">' +
-                    '<textarea placeholder="Your text"></textarea>' +
+                '<textarea placeholder="Your text"></textarea>' +
                 '</article>' +
                 '</div>');
 
@@ -402,12 +402,12 @@ export class ScreenshotView {
                 containment: containmentSelector,
             });
 
-            stickerContainer.find('a.remove').on('click', function() {
+            stickerContainer.find('a.remove').on('click', function () {
                 jQuery(this).closest('.sticker-container').remove();
             });
-            stickerContainer.find('a.edit').on('click', function() {
+            stickerContainer.find('a.edit').on('click', function () {
                 var textContainer = stickerContainer.find('article.text-container');
-                if(textContainer.is(":visible")) {
+                if (textContainer.is(":visible")) {
                     textContainer.hide();
                 } else {
                     textContainer.show();
@@ -417,7 +417,7 @@ export class ScreenshotView {
             myThis.screenshotPreviewElement.append(stickerContainer);
         });
 
-        this.container.find('.screenshot-text-2').off('click').on('click', function(event) {
+        this.container.find('.screenshot-text-2').off('click').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -427,16 +427,45 @@ export class ScreenshotView {
             var stickerContainer = jQuery('<div class="sticker-container text-2">' +
                 '<textarea placeholder="Your text"></textarea>' +
                 '<a class="remove"><img src="dist/img/ic_remove_circle_red_shadow_24px.png" /></a>' +
+                '<a class="color" style="background-image: url(\'dist/img/screenshot_button_background.png\');"><i class="material-icons">format_color_text</i></a>' +
                 '</div>');
+
+            var textarea = stickerContainer.find('textarea');
+            var colorLink = stickerContainer.find('a.color');
+            var defaultColor = "#000";
+            textarea.css('color', defaultColor);
 
             stickerContainer.css('width', 'auto');
             stickerContainer.css('height', 'auto');
+
+            stickerContainer.find('a.color').spectrum({
+                color: defaultColor,
+                showPaletteOnly: true,
+                togglePaletteOnly: true,
+                togglePaletteMoreText: 'more',
+                togglePaletteLessText: 'less',
+                palette: [
+                    ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
+                    ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
+                    ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
+                    ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
+                    ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
+                    ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
+                    ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
+                    ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
+                ],
+                change: function (color) {
+                    var color = color.toHexString();
+                    textarea.css('color', color);
+                    colorLink.css('color', color);
+                }
+            });
 
             var containmentSelector = '#' + myThis.container.attr('id') + ' .screenshot-preview';
 
             stickerContainer.resizable({
                 containment: containmentSelector,
-                resize: function() {
+                resize: function () {
                     textArea.css('height', (stickerContainer.height() - 40) + 'px');
                     textArea.css('width', (stickerContainer.width() - 40) + 'px');
                 }
@@ -445,15 +474,15 @@ export class ScreenshotView {
                 cursor: "crosshair",
                 containment: containmentSelector,
             });
-            stickerContainer.find('a.remove').on('click', function() {
+            stickerContainer.find('a.remove').on('click', function () {
                 jQuery(this).closest('.sticker-container').remove();
             });
 
             myThis.screenshotPreviewElement.append(stickerContainer);
 
             var textArea = stickerContainer.find('textarea:first');
-            textArea.on('keydown', function() {
-                setTimeout(function() {
+            textArea.on('keydown', function () {
+                setTimeout(function () {
                     textArea.css('height', textArea[0].scrollHeight + 'px');
                     stickerContainer.css('height', textArea[0].scrollHeight + 40 + 'px');
                 }, 1);
@@ -461,19 +490,19 @@ export class ScreenshotView {
             textArea.focus();
         });
 
-        this.container.find('.screenshot-draw-undo').on('click', function(event) {
+        this.container.find('.screenshot-draw-undo').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
             myThis.undoOperation();
         });
 
-        this.container.find('.screenshot-draw-remove').on('click', function(event) {
+        this.container.find('.screenshot-draw-remove').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
             myThis.reset();
         });
 
-        this.container.find('.screenshot-operation.sticking').off('click').on('click', function(event) {
+        this.container.find('.screenshot-operation.sticking').off('click').on('click', function (event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -500,8 +529,8 @@ export class ScreenshotView {
                 containment: containmentSelector,
             });
 
-            stickerContainer.find('a.remove').on('click', function() {
-               jQuery(this).closest('.sticker-container').remove();
+            stickerContainer.find('a.remove').on('click', function () {
+                jQuery(this).closest('.sticker-container').remove();
             });
 
             myThis.screenshotPreviewElement.append(stickerContainer);
