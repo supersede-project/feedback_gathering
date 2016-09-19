d11e0f15a0a2d0377a101406ba77694a127cefa3
rschaniel <ronnieschaniel@gmail.com>
2016-09-19 15:24:35 +0200

    new svg stickers working

--- web_library/app/views/screenshot/screenshot_view.js
+++ web_library/app/views/screenshot/screenshot_view.js
@@ -11,6 +11,7 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
     var textMode2 = 'textMode2';
     var black = "#000000";
     var red = "#FF0000";
+    var defaultColor = black;
     var ScreenshotView = (function () {
         function ScreenshotView(screenshotMechanism, screenshotPreviewElement, screenshotCaptureButton, elementToCapture, container, distPath, elementsToHide) {
             this.screenshotMechanism = screenshotMechanism;
@@ -57,32 +58,126 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
                     var screenshotCaptureButtonActiveText = myThis.screenshotCaptureButton.data('active-text');
                     myThis.screenshotCaptureButton.text(screenshotCaptureButtonActiveText);
                     myThis.initDrawing();
-                    var fabricCanvas = new fabric.Canvas(canvasId);
-                    var oldCanvas = new fabric.Image(img, { width: canvas.width, height: canvas.height });
-                    var rect = new fabric.Rect({
-                        left: 100,
-                        top: 100,
-                        fill: 'red',
-                        width: 20,
-                        height: 20
-                    });
-                    oldCanvas.set('selectable', false);
-                    oldCanvas.set('hoverCursor', 'default');
-                    fabricCanvas.add(oldCanvas);
-                    fabricCanvas.add(rect);
-                    fabric.loadSVGFromURL(myThis.distPath + 'img/ic_sentiment_satisfied_black_24px.svg', function (objects, options) {
-                        var obj = fabric.util.groupSVGElements(objects, options);
-                        for (var _i = 0, _a = obj.paths; _i < _a.length; _i++) {
-                            var path = _a[_i];
-                            if (path.getFill() != "") {
-                                console.log(path.getFill());
-                                path.setFill('#00ff00');
+                    myThis.initFabric(canvasId, img, canvas);
+                    myThis.initSVGStickers();
+                }
+            });
+        };
+        ScreenshotView.prototype.initFabric = function (canvasId, img, canvas) {
+            var myThis = this;
+            this.fabricCanvas = new fabric.Canvas(canvasId);
+            var oldCanvas = new fabric.Image(img, { width: canvas.width, height: canvas.height });
+            oldCanvas.set('selectable', false);
+            oldCanvas.set('hoverCursor', 'default');
+            this.fabricCanvas.add(oldCanvas);
+            fabric.Object.prototype.set({
+                transparentCorners: true,
+                borderColor: '#000000',
+                cornerColor: '#545454',
+                cornerSize: 9,
+                padding: 10
+            });
+            var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selectedObjectControls');
+            selectedObjectControls.hide();
+            myThis.fabricCanvas.on('object:selected', function (e) {
+                var selectedObject = e.target;
+                if (selectedObject.get('type') !== 'path-group') {
+                    var currentObjectColor = selectedObject.getFill();
+                }
+                else {
+                    for (var _i = 0, _a = selectedObject.paths; _i < _a.length; _i++) {
+                        var path = _a[_i];
+                        if (path.getFill() != "") {
+                            var currentObjectColor = path.getFill();
+                            break;
+                        }
+                    }
+                }
+                selectedObjectControls.show();
+                selectedObjectControls.find('.delete').off().on('click', function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if (selectedObject) {
+                        selectedObject.remove();
+                    }
+                });
+                selectedObjectControls.find('a.color').css('color', currentObjectColor);
+                selectedObjectControls.find('a.color').spectrum({
+                    color: defaultColor,
+                    showPaletteOnly: true,
+                    togglePaletteOnly: true,
+                    togglePaletteMoreText: 'more',
+                    togglePaletteLessText: 'less',
+                    palette: [
+                        ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
+                        ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
+                        ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
+                        ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
+                        ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
+                        ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
+                        ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
+                        ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
+                    ],
+                    change: function (color) {
+                        var color = color.toHexString();
+                        jQuery(this).css('color', color);
+                        if (selectedObject.get('type') !== 'path-group') {
+                            selectedObject.setFill(color);
+                        }
+                        else {
+                            for (var _i = 0, _a = selectedObject.paths; _i < _a.length; _i++) {
+                                var path = _a[_i];
+                                if (path.getFill() != "") {
+                                    path.setFill(color);
+                                }
                             }
                         }
-                        fabricCanvas.add(obj).renderAll();
+                        myThis.fabricCanvas.renderAll();
+                    }
+                });
+            });
+            myThis.fabricCanvas.on('selection:cleared', function () {
+                var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selectedObjectControls');
+                selectedObjectControls.hide();
+                selectedObjectControls.find('.delete').off();
+                selectedObjectControls.find('.color').off();
+            });
+        };
+        ScreenshotView.prototype.initTextAnnotation = function () {
+            var text = new fabric.IText('Your text', { left: 100, top: 100, fontFamily: 'arial black' });
+            this.fabricCanvas.add(text);
+        };
+        ScreenshotView.prototype.initSVGStickers = function () {
+            var myThis = this;
+            myThis.container.find('.svg-sticker-source').draggable({
+                cursor: "crosshair",
+                revert: "invalid",
+                helper: "clone",
+                zIndex: 5000,
+                drag: function (event, ui) {
+                    myThis.screenshotPreviewElement.css('border-style', 'dashed');
+                },
+                stop: function (event, ui) {
+                    myThis.screenshotPreviewElement.css('border-style', 'solid');
+                }
+            }).on('mouseover mouseenter', function () {
+                myThis.screenshotPreviewElement.css('border-style', 'dashed');
+            }).on('mouseleave', function () {
+                myThis.screenshotPreviewElement.css('border-style', 'solid');
+            });
+            myThis.screenshotPreviewElement.droppable({
+                drop: function (event, ui) {
+                    var sticker = $(ui.helper);
+                    var offsetY = event.pageY - $(this).offset().top;
+                    var offsetX = event.pageX - $(this).offset().left;
+                    offsetY -= 12;
+                    offsetX -= 12;
+                    fabric.loadSVGFromURL(sticker.attr('src'), function (objects, options) {
+                        var obj = fabric.util.groupSVGElements(objects, options);
+                        obj.set('left', offsetX);
+                        obj.set('top', offsetY);
+                        myThis.fabricCanvas.add(obj).renderAll();
                     });
-                    var text = new fabric.IText('Your text', { left: 100, top: 100, fontFamily: 'arial black' });
-                    fabricCanvas.add(text);
                 }
             });
         };
@@ -464,83 +559,6 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
                     jQuery(this).closest('.sticker-container').remove();
                 });
                 myThis.screenshotPreviewElement.append(stickerContainer);
-            });
-            this.container.find('.sticker-source').draggable({
-                cursor: "crosshair",
-                revert: "invalid",
-                helper: "clone",
-                zIndex: 5000
-            });
-            this.screenshotPreviewElement.droppable({
-                drop: function (event, ui) {
-                    if (!ui.helper.hasClass('sticker-container')) {
-                        var stickerClone = $(ui.helper).clone();
-                        var stickerContainer = jQuery('<div class="sticker-container">' +
-                            '<a class="remove"><img src="' + myThis.distPath + 'img/ic_remove_circle_red_shadow_24px.png" /></a>' +
-                            '<a class="color bottom-left" style="background-image: url(\'' + myThis.distPath + 'img/screenshot_button_background.png\');"><i class="material-icons">format_color_fill</i></a>' +
-                            '</div>');
-                        var offsetCorrection = 12 * 2;
-                        var offsetY = event.pageY - $(this).offset().top - offsetCorrection;
-                        var offsetX = event.pageX - $(this).offset().left - offsetCorrection;
-                        stickerContainer.css('width', 'auto');
-                        stickerContainer.css('height', 'auto');
-                        stickerContainer.css('top', offsetY + 'px');
-                        stickerContainer.css('left', offsetX + 'px');
-                        stickerContainer.css('padding', '20px');
-                        stickerClone.css('top', 0);
-                        stickerClone.css('left', 0);
-                        stickerClone.addClass('color-sticker');
-                        stickerContainer.append(stickerClone);
-                        var containmentSelector = '#' + myThis.container.attr('id') + ' .screenshot-preview';
-                        var initDiagonal;
-                        var initFontSize;
-                        stickerContainer.resizable({
-                            containment: containmentSelector,
-                            aspectRatio: true,
-                            create: function (event, ui) {
-                                initDiagonal = myThis.getContentDiagonal(stickerContainer);
-                                initFontSize = parseInt(stickerContainer.find('i.color-sticker').css("font-size"));
-                            },
-                            resize: function (e, ui) {
-                                var newDiagonal = myThis.getContentDiagonal(stickerContainer);
-                                var ratio = newDiagonal / initDiagonal;
-                                stickerContainer.find('i.color-sticker').css("font-size", initFontSize + ratio * 3);
-                            }
-                        });
-                        stickerContainer.draggable({
-                            cursor: "crosshair",
-                            containment: containmentSelector,
-                        });
-                        stickerContainer.find('a.remove').on('click', function () {
-                            jQuery(this).closest('.sticker-container').remove();
-                        });
-                        var defaultColor = "#000";
-                        stickerClone.css('color', defaultColor);
-                        stickerContainer.find('a.color').spectrum({
-                            color: defaultColor,
-                            showPaletteOnly: true,
-                            togglePaletteOnly: true,
-                            togglePaletteMoreText: 'more',
-                            togglePaletteLessText: 'less',
-                            palette: [
-                                ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
-                                ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
-                                ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
-                                ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
-                                ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
-                                ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
-                                ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
-                                ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
-                            ],
-                            change: function (color) {
-                                var color = color.toHexString();
-                                stickerClone.css('color', color);
-                                stickerContainer.find('a.color').css('color', color);
-                            }
-                        });
-                        $(this).append(stickerContainer);
-                    }
-                }
             });
             this.container.find('.screenshot-operations').show();
         };
--- web_library/app/views/screenshot/screenshot_view.ts
+++ web_library/app/views/screenshot/screenshot_view.ts
@@ -15,6 +15,7 @@ const textMode:string = 'textMode';
 const textMode2:string = 'textMode2';
 const black:string = "#000000";
 const red:string = "#FF0000";
+const defaultColor:string = black;
 
 
 export class ScreenshotView {
@@ -38,6 +39,8 @@ export class ScreenshotView {
     canvasOriginalHeight:number;
     container:JQuery;
     distPath:string;
+    fabricCanvas;
+
 
     constructor(screenshotMechanism:Mechanism, screenshotPreviewElement:JQuery, screenshotCaptureButton:JQuery,
                 elementToCapture:JQuery, container:JQuery, distPath:string, elementsToHide?:any) {
@@ -92,45 +95,143 @@ export class ScreenshotView {
                 myThis.screenshotCanvas = canvas;
                 img.src = data;
 
-
-
                 let screenshotCaptureButtonActiveText = myThis.screenshotCaptureButton.data('active-text');
                 myThis.screenshotCaptureButton.text(screenshotCaptureButtonActiveText);
                 myThis.initDrawing();
 
+                myThis.initFabric(canvasId, img, canvas);
+                myThis.initSVGStickers();
+            }
+        });
+    }
 
+    initFabric(canvasId, img, canvas) {
+        var myThis = this;
+        this.fabricCanvas = new fabric.Canvas(canvasId);
+        var oldCanvas = new fabric.Image(img, { width: canvas.width, height: canvas.height });
 
-                var fabricCanvas = new fabric.Canvas(canvasId);
-                var oldCanvas = new fabric.Image(img, { width: canvas.width, height: canvas.height });
+        oldCanvas.set('selectable', false);
+        oldCanvas.set('hoverCursor', 'default');
+        this.fabricCanvas.add(oldCanvas);
 
-                var rect = new fabric.Rect({
-                    left: 100,
-                    top: 100,
-                    fill: 'red',
-                    width: 20,
-                    height: 20
-                });
-                oldCanvas.set('selectable', false);
-                oldCanvas.set('hoverCursor', 'default');
-                fabricCanvas.add(oldCanvas);
-                fabricCanvas.add(rect);
+        fabric.Object.prototype.set({
+            transparentCorners: true,
+            borderColor: '#000000',
+            cornerColor: '#545454',
+            cornerSize: 9,
+            padding: 10
+        });
 
-                fabric.loadSVGFromURL(myThis.distPath + 'img/ic_sentiment_satisfied_black_24px.svg', function(objects, options) {
-                    var obj = fabric.util.groupSVGElements(objects, options);
+        var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selectedObjectControls');
+        selectedObjectControls.hide();
 
-                    for(var path of obj.paths) {
-                        if(path.getFill() != "") {
-                            console.log(path.getFill());
-                            path.setFill('#00ff00');
+        myThis.fabricCanvas.on('object:selected', function(e) {
+            var selectedObject = e.target;
+
+            if(selectedObject.get('type') !== 'path-group') {
+                var currentObjectColor = selectedObject.getFill();
+            } else {
+                for(var path of selectedObject.paths) {
+                    if(path.getFill() != "") {
+                        var currentObjectColor = path.getFill();
+                        break;
+                    }
+                }
+            }
+
+            selectedObjectControls.show();
+            selectedObjectControls.find('.delete').off().on('click', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                if(selectedObject) {
+                    selectedObject.remove();
+                }
+            });
+
+            selectedObjectControls.find('a.color').css('color', currentObjectColor);
+            selectedObjectControls.find('a.color').spectrum({
+                color: defaultColor,
+                showPaletteOnly: true,
+                togglePaletteOnly: true,
+                togglePaletteMoreText: 'more',
+                togglePaletteLessText: 'less',
+                palette: [
+                    ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
+                    ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
+                    ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
+                    ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
+                    ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
+                    ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
+                    ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
+                    ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
+                ],
+                change: function (color) {
+                    var color = color.toHexString();
+                    jQuery(this).css('color', color);
+
+                    if(selectedObject.get('type') !== 'path-group') {
+                        selectedObject.setFill(color);
+                    } else {
+                        for(var path of selectedObject.paths) {
+                            if(path.getFill() != "") {
+                                path.setFill(color);
+                            }
                         }
                     }
+                    myThis.fabricCanvas.renderAll();
+                }
+            });
+        });
 
-                    fabricCanvas.add(obj).renderAll();
+        myThis.fabricCanvas.on('selection:cleared', function() {
+            var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selectedObjectControls');
+            selectedObjectControls.hide();
+            selectedObjectControls.find('.delete').off();
+            selectedObjectControls.find('.color').off();
+        });
+
+    }
+
+    initTextAnnotation() {
+        var text = new fabric.IText('Your text', { left: 100, top: 100, fontFamily: 'arial black' });
+        this.fabricCanvas.add(text);
+    }
+
+    initSVGStickers() {
+        var myThis = this;
+        myThis.container.find('.svg-sticker-source').draggable({
+            cursor: "crosshair",
+            revert: "invalid",
+            helper: "clone",
+            zIndex: 5000,
+            drag: function(event:Event, ui) {
+                myThis.screenshotPreviewElement.css('border-style', 'dashed');
+            },
+            stop: function(event:DragEvent, ui) {
+                myThis.screenshotPreviewElement.css('border-style', 'solid');
+            }
+        }).on('mouseover mouseenter', function() {
+            myThis.screenshotPreviewElement.css('border-style', 'dashed');
+        }).on('mouseleave', function() {
+            myThis.screenshotPreviewElement.css('border-style', 'solid');
+        });
+
+        myThis.screenshotPreviewElement.droppable({
+            drop: function (event:DragEvent, ui) {
+                var sticker = $(ui.helper);
+
+                var offsetY = event.pageY - $(this).offset().top;
+                var offsetX = event.pageX - $(this).offset().left;
+
+                offsetY -= 12;
+                offsetX -= 12;
+
+                fabric.loadSVGFromURL(sticker.attr('src'), function(objects, options) {
+                    var obj = fabric.util.groupSVGElements(objects, options);
+                    obj.set('left', offsetX);
+                    obj.set('top', offsetY);
+                    myThis.fabricCanvas.add(obj).renderAll();
                 });
-
-
-                var text = new fabric.IText('Your text', { left: 100, top: 100, fontFamily: 'arial black' });
-                fabricCanvas.add(text);
             }
         });
     }
@@ -577,96 +678,6 @@ export class ScreenshotView {
             });
 
             myThis.screenshotPreviewElement.append(stickerContainer);
-        });
-
-        this.container.find('.sticker-source').draggable({
-            cursor: "crosshair",
-            revert: "invalid",
-            helper: "clone",
-            zIndex: 5000
-        });
-
-        this.screenshotPreviewElement.droppable({
-            drop: function(event:DragEvent, ui) {
-                if(!ui.helper.hasClass('sticker-container')) {
-                    var stickerClone = $(ui.helper).clone();
-
-                    var stickerContainer = jQuery('<div class="sticker-container">' +
-                        '<a class="remove"><img src="' + myThis.distPath + 'img/ic_remove_circle_red_shadow_24px.png" /></a>' +
-                        '<a class="color bottom-left" style="background-image: url(\'' + myThis.distPath + 'img/screenshot_button_background.png\');"><i class="material-icons">format_color_fill</i></a>' +
-                        '</div>');
-
-                    // TODO get this offsetCorrection somehow from the stickerClone size
-                    var offsetCorrection = 12 * 2;
-                    var offsetY = event.pageY - $(this).offset().top - offsetCorrection;
-                    var offsetX = event.pageX - $(this).offset().left - offsetCorrection;
-
-                    stickerContainer.css('width', 'auto');
-                    stickerContainer.css('height', 'auto');
-                    stickerContainer.css('top', offsetY + 'px');
-                    stickerContainer.css('left', offsetX + 'px');
-                    stickerContainer.css('padding', '20px');
-                    stickerClone.css('top', 0);
-                    stickerClone.css('left', 0);
-                    stickerClone.addClass('color-sticker');
-                    stickerContainer.append(stickerClone);
-
-                    var containmentSelector = '#' + myThis.container.attr('id') + ' .screenshot-preview';
-
-                    var initDiagonal;
-                    var initFontSize;
-
-                    stickerContainer.resizable({
-                        containment: containmentSelector,
-                        aspectRatio: true,
-                        create: function(event, ui) {
-                            initDiagonal = myThis.getContentDiagonal(stickerContainer);
-                            initFontSize = parseInt(stickerContainer.find('i.color-sticker').css("font-size"));
-                        },
-                        resize: function(e, ui) {
-                            var newDiagonal = myThis.getContentDiagonal(stickerContainer);
-                            var ratio = newDiagonal / initDiagonal;
-
-                            stickerContainer.find('i.color-sticker').css("font-size", initFontSize + ratio * 3);
-                        }
-                    });
-                    stickerContainer.draggable({
-                        cursor: "crosshair",
-                        containment: containmentSelector,
-                    });
-
-                    stickerContainer.find('a.remove').on('click', function () {
-                        jQuery(this).closest('.sticker-container').remove();
-                    });
-
-                    var defaultColor = "#000";
-                    stickerClone.css('color', defaultColor);
-                    stickerContainer.find('a.color').spectrum({
-                        color: defaultColor,
-                        showPaletteOnly: true,
-                        togglePaletteOnly: true,
-                        togglePaletteMoreText: 'more',
-                        togglePaletteLessText: 'less',
-                        palette: [
-                            ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
-                            ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
-                            ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
-                            ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
-                            ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
-                            ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
-                            ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
-                            ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
-                        ],
-                        change: function (color) {
-                            var color = color.toHexString();
-                            stickerClone.css('color', color);
-                            stickerContainer.find('a.color').css('color', color);
-                        }
-                    });
-
-                    $(this).append(stickerContainer);
-                }
-            }
         });
 
         this.container.find('.screenshot-operations').show();
