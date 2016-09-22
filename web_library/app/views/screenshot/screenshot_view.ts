f598509cf8aeb1beec95adb5a249c27aec672416
rschaniel <ronnieschaniel@gmail.com>
2016-09-22 10:53:16 +0200

    freehand drawing

--- web_library/app/views/screenshot/screenshot_view.ts
+++ web_library/app/views/screenshot/screenshot_view.ts
@@ -100,6 +100,7 @@ export class ScreenshotView {
                 };
 
                 myThis.initFabric(img, canvas);
+                myThis.initFreehandDrawing();
                 myThis.initSVGStickers();
                 myThis.initScreenshotOperations();
 
@@ -128,7 +129,7 @@ export class ScreenshotView {
             padding: 1
         });
 
-        var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selectedObjectControls');
+        var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selected-object-controls');
         selectedObjectControls.hide();
 
         myThis.fabricCanvas.on('object:selected', function (e) {
@@ -190,7 +191,7 @@ export class ScreenshotView {
         });
 
         myThis.fabricCanvas.on('selection:cleared', function () {
-            var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selectedObjectControls');
+            var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selected-object-controls');
             selectedObjectControls.hide();
             selectedObjectControls.find('.delete').off();
             selectedObjectControls.find('.color').off();
@@ -198,7 +199,7 @@ export class ScreenshotView {
     }
 
     determineCanvasScaleForRetinaDisplay() {
-        if(window.devicePixelRatio !== 1) {
+        if (window.devicePixelRatio !== 1) {
             var height = jQuery('.screenshot-preview canvas').height();
             var width = jQuery('.screenshot-preview canvas').width();
 
@@ -207,7 +208,7 @@ export class ScreenshotView {
             // TODO get this working!!!
             canvas.setAttribute('width', window.devicePixelRatio * width);
             canvas.setAttribute('height', window.devicePixelRatio * height);
-            canvas.setAttribute('style', 'width="' + width +'px"; height="' + height + 'px";');
+            canvas.setAttribute('style', 'width="' + width + 'px"; height="' + height + 'px";');
 
             canvas.getContext('2d').scale(window.devicePixelRatio, window.devicePixelRatio);
         }
@@ -337,6 +338,62 @@ export class ScreenshotView {
         text.selectAll();
     }
 
+    initFreehandDrawing() {
+        var myThis = this;
+        var currentFreehandDrawingColor = "#000000";
+        var freehandControls = jQuery('.freehand-controls');
+        freehandControls.hide();
+
+        myThis.fabricCanvas.freeDrawingBrush.width = 6;
+
+        freehandControls.find('a.color').css('color', currentFreehandDrawingColor);
+        freehandControls.find('a.color').spectrum({
+            color: defaultColor,
+            showPaletteOnly: true,
+            togglePaletteOnly: true,
+            togglePaletteMoreText: 'more',
+            togglePaletteLessText: 'less',
+            palette: [
+                ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
+                ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
+                ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
+                ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
+                ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
+                ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
+                ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
+                ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
+            ],
+            change: function (color) {
+                var color = color.toHexString();
+                jQuery(this).css('color', color);
+                myThis.fabricCanvas.freeDrawingBrush.color = color;
+                myThis.fabricCanvas.renderAll();
+            }
+        });
+
+        jQuery('.screenshot-operations .freehand').on('click', function () {
+            if (myThis.fabricCanvas.isDrawingMode) {
+                myThis.disableFreehandDrawing();
+            } else {
+                myThis.enableFreehandDrawing();
+            }
+        });
+    }
+
+    enableFreehandDrawing() {
+        var freehandControls = jQuery('.freehand-controls');
+        this.fabricCanvas.isDrawingMode = true;
+        jQuery('.screenshot-operations .freehand').css('border-bottom', '1px solid black');
+        freehandControls.show();
+    }
+
+    disableFreehandDrawing() {
+        var freehandControls = jQuery('.freehand-controls');
+        jQuery('.screenshot-operations .freehand').css('border-bottom', 'none');
+        this.fabricCanvas.isDrawingMode = false;
+        freehandControls.hide();
+    }
+
     initSVGStickers() {
         var myThis = this;
         myThis.container.find('.svg-sticker-source').draggable({
@@ -345,6 +402,7 @@ export class ScreenshotView {
             helper: "clone",
             zIndex: 5000,
             drag: function (event:Event, ui) {
+                myThis.disableFreehandDrawing();
                 myThis.screenshotPreviewElement.css('border-style', 'dashed');
             },
             stop: function (event:DragEvent, ui) {
