7bddf2c72e6858393beb5061763e0d67c138d9b9
rschaniel <ronnieschaniel@gmail.com>
2016-09-22 21:03:20 +0200

    color option right on screenshot object

--- web_library/app/views/screenshot/screenshot_view.js
+++ web_library/app/views/screenshot/screenshot_view.js
@@ -1,4 +1,4 @@
-define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/data_helper', './canvas_state', '../../js/lib/html2canvas.js', 'fabric'], function (require, exports, screenshot_view_drawing_1, data_helper_1, canvas_state_1) {
+define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/data_helper', './canvas_state', '../../js/lib/html2canvas.js'], function (require, exports, screenshot_view_drawing_1, data_helper_1, canvas_state_1) {
     "use strict";
     var freehandDrawingMode = 'freehandDrawingMode';
     var rectDrawingMode = 'rectDrawingMode';
@@ -62,6 +62,7 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
                     myThis.initFreehandDrawing();
                     myThis.initSVGStickers();
                     myThis.initScreenshotOperations();
+                    myThis.customizeControls();
                     var screenshotCaptureButtonActiveText = myThis.screenshotCaptureButton.data('active-text');
                     myThis.screenshotCaptureButton.text(screenshotCaptureButtonActiveText);
                 }
@@ -75,17 +76,11 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
             pageScreenshotCanvas.set('selectable', false);
             pageScreenshotCanvas.set('hoverCursor', 'default');
             this.fabricCanvas.add(pageScreenshotCanvas);
-            fabric.Object.prototype.set({
-                transparentCorners: true,
-                borderColor: '#000000',
-                cornerColor: '#545454',
-                cornerSize: 9,
-                padding: 1
-            });
             var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selected-object-controls');
             selectedObjectControls.hide();
             myThis.fabricCanvas.on('object:selected', function (e) {
                 var selectedObject = e.target;
+                selectedObjectControls.show();
                 if (selectedObject.get('type') !== 'path-group') {
                     var currentObjectColor = selectedObject.getFill();
                 }
@@ -98,7 +93,6 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
                         }
                     }
                 }
-                selectedObjectControls.show();
                 selectedObjectControls.find('.delete').off().on('click', function (e) {
                     e.preventDefault();
                     e.stopPropagation();
@@ -107,7 +101,7 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
                     }
                 });
                 selectedObjectControls.find('a.color').css('color', currentObjectColor);
-                selectedObjectControls.find('a.color').spectrum({
+                selectedObjectControls.find('a.color').off().spectrum({
                     color: defaultColor,
                     showPaletteOnly: true,
                     togglePaletteOnly: true,
@@ -269,8 +263,8 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
             var freehandControls = jQuery('.freehand-controls');
             freehandControls.hide();
             myThis.fabricCanvas.freeDrawingBrush.width = 6;
-            freehandControls.find('a.color').css('color', currentFreehandDrawingColor);
-            freehandControls.find('a.color').spectrum({
+            freehandControls.find('a.freehand-color').css('color', currentFreehandDrawingColor);
+            freehandControls.find('a.freehand-color').off().spectrum({
                 color: defaultColor,
                 showPaletteOnly: true,
                 togglePaletteOnly: true,
@@ -348,6 +342,7 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
                             var svgObject = fabric.util.groupSVGElements(objects, options);
                             svgObject.set('left', offsetX);
                             svgObject.set('top', offsetY);
+                            svgObject.scale(3);
                             myThis.fabricCanvas.add(svgObject).renderAll();
                             myThis.fabricCanvas.setActiveObject(svgObject);
                         });
@@ -439,6 +434,83 @@ define(["require", "exports", './screenshot_view_drawing', '../../js/helpers/dat
         };
         ScreenshotView.prototype.disableAllScreenshotOperations = function () {
             this.container.find('button.screenshot-operation').removeClass('active');
+        };
+        ScreenshotView.prototype.customizeControls = function () {
+            var myThis = this;
+            var colorLinkElement = jQuery('<a class="corner-color" href="#"><i class="material-icons">format_color_fill</i></a>');
+            colorLinkElement.css('position', 'absolute');
+            colorLinkElement.css('color', defaultColor);
+            colorLinkElement.css('width', '16px');
+            colorLinkElement.css('height', '16px');
+            colorLinkElement.css('opacity', '0');
+            var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selected-object-controls');
+            fabric.Canvas.prototype.customiseControls({
+                mt: {
+                    action: function (e, target) {
+                        colorLinkElement.css('top', e.offsetY - 12 + 'px');
+                        colorLinkElement.css('left', e.offsetX - 8 + 'px');
+                        colorLinkElement.off().spectrum({
+                            color: defaultColor,
+                            showPaletteOnly: true,
+                            togglePaletteOnly: true,
+                            togglePaletteMoreText: 'more',
+                            togglePaletteLessText: 'less',
+                            palette: [
+                                ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
+                                ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
+                                ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
+                                ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
+                                ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
+                                ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
+                                ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
+                                ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
+                            ],
+                            change: function (color) {
+                                var color = color.toHexString();
+                                jQuery(this).css('color', color);
+                                if (target.get('type') !== 'path-group') {
+                                    target.setFill(color);
+                                }
+                                else {
+                                    for (var _i = 0, _a = target.paths; _i < _a.length; _i++) {
+                                        var path = _a[_i];
+                                        if (path.getFill() != "") {
+                                            path.setFill(color);
+                                        }
+                                    }
+                                }
+                                selectedObjectControls.find('a.color').css('color', color);
+                                myThis.fabricCanvas.renderAll();
+                                jQuery(this).remove();
+                            }
+                        });
+                        myThis.screenshotPreviewElement.append(colorLinkElement);
+                        colorLinkElement.click();
+                    },
+                    cursor: 'pointer'
+                },
+                ml: {
+                    action: function (e, target) {
+                        target.remove();
+                        myThis.fabricCanvas.renderAll();
+                    },
+                    cursor: 'pointer'
+                }
+            });
+            fabric.Object.prototype.customiseCornerIcons({
+                settings: {
+                    borderColor: 'black',
+                    cornerSize: 24,
+                    cornerShape: 'rect',
+                    cornerPadding: 1
+                },
+                ml: {
+                    icon: myThis.distPath + 'img/ic_delete_black_24px_background.svg'
+                },
+                mt: {
+                    icon: myThis.distPath + 'img/ic_format_color_fill_black_24px_background.svg'
+                }
+            });
         };
         return ScreenshotView;
     }());
--- web_library/app/views/screenshot/screenshot_view.ts
+++ web_library/app/views/screenshot/screenshot_view.ts
@@ -2,7 +2,6 @@ import {ScreenshotViewDrawing} from './screenshot_view_drawing';
 import {DataHelper} from '../../js/helpers/data_helper';
 import '../../js/lib/html2canvas.js';
 import {Mechanism} from '../../models/mechanisms/mechanism';
-import 'fabric';
 import {CanvasState} from './canvas_state';
 
 const freehandDrawingMode:string = 'freehandDrawingMode';
@@ -103,6 +102,7 @@ export class ScreenshotView {
                 myThis.initFreehandDrawing();
                 myThis.initSVGStickers();
                 myThis.initScreenshotOperations();
+                myThis.customizeControls();
 
                 let screenshotCaptureButtonActiveText = myThis.screenshotCaptureButton.data('active-text');
                 myThis.screenshotCaptureButton.text(screenshotCaptureButtonActiveText);
@@ -121,19 +121,12 @@ export class ScreenshotView {
         pageScreenshotCanvas.set('hoverCursor', 'default');
         this.fabricCanvas.add(pageScreenshotCanvas);
 
-        fabric.Object.prototype.set({
-            transparentCorners: true,
-            borderColor: '#000000',
-            cornerColor: '#545454',
-            cornerSize: 9,
-            padding: 1
-        });
-
         var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selected-object-controls');
         selectedObjectControls.hide();
 
         myThis.fabricCanvas.on('object:selected', function (e) {
             var selectedObject = e.target;
+            selectedObjectControls.show();
 
             if (selectedObject.get('type') !== 'path-group') {
                 var currentObjectColor = selectedObject.getFill();
@@ -146,7 +139,6 @@ export class ScreenshotView {
                 }
             }
 
-            selectedObjectControls.show();
             selectedObjectControls.find('.delete').off().on('click', function (e) {
                 e.preventDefault();
                 e.stopPropagation();
@@ -156,7 +148,7 @@ export class ScreenshotView {
             });
 
             selectedObjectControls.find('a.color').css('color', currentObjectColor);
-            selectedObjectControls.find('a.color').spectrum({
+            selectedObjectControls.find('a.color').off().spectrum({
                 color: defaultColor,
                 showPaletteOnly: true,
                 togglePaletteOnly: true,
@@ -346,8 +338,8 @@ export class ScreenshotView {
 
         myThis.fabricCanvas.freeDrawingBrush.width = 6;
 
-        freehandControls.find('a.color').css('color', currentFreehandDrawingColor);
-        freehandControls.find('a.color').spectrum({
+        freehandControls.find('a.freehand-color').css('color', currentFreehandDrawingColor);
+        freehandControls.find('a.freehand-color').off().spectrum({
             color: defaultColor,
             showPaletteOnly: true,
             togglePaletteOnly: true,
@@ -431,6 +423,7 @@ export class ScreenshotView {
                         var svgObject = fabric.util.groupSVGElements(objects, options);
                         svgObject.set('left', offsetX);
                         svgObject.set('top', offsetY);
+                        svgObject.scale(3);
                         myThis.fabricCanvas.add(svgObject).renderAll();
                         myThis.fabricCanvas.setActiveObject(svgObject);
                     });
@@ -539,6 +532,86 @@ export class ScreenshotView {
 
     disableAllScreenshotOperations() {
         this.container.find('button.screenshot-operation').removeClass('active');
+    }
+
+    customizeControls() {
+        var myThis = this;
+
+        var colorLinkElement = jQuery('<a class="corner-color" href="#"><i class="material-icons">format_color_fill</i></a>');
+        colorLinkElement.css('position', 'absolute');
+        colorLinkElement.css('color', defaultColor);
+        colorLinkElement.css('width', '16px');
+        colorLinkElement.css('height', '16px');
+        colorLinkElement.css('opacity', '0');
+
+        var selectedObjectControls = jQuery('#screenshotMechanism' + myThis.screenshotMechanism.id + ' .selected-object-controls');
+
+        fabric.Canvas.prototype.customiseControls({
+            mt: {
+                action: function (e, target) {
+                    colorLinkElement.css('top', e.offsetY - 12 + 'px');
+                    colorLinkElement.css('left', e.offsetX - 8 + 'px');
+                    colorLinkElement.off().spectrum({
+                        color: defaultColor,
+                        showPaletteOnly: true,
+                        togglePaletteOnly: true,
+                        togglePaletteMoreText: 'more',
+                        togglePaletteLessText: 'less',
+                        palette: [
+                            ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
+                            ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
+                            ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
+                            ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
+                            ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
+                            ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
+                            ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
+                            ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
+                        ],
+                        change: function (color) {
+                            var color = color.toHexString();
+                            jQuery(this).css('color', color);
+                            if (target.get('type') !== 'path-group') {
+                                target.setFill(color);
+                            } else {
+                                for (var path of target.paths) {
+                                    if (path.getFill() != "") {
+                                        path.setFill(color);
+                                    }
+                                }
+                            }
+                            selectedObjectControls.find('a.color').css('color', color);
+                            myThis.fabricCanvas.renderAll();
+                            jQuery(this).remove();
+                        }
+                    });
+                    myThis.screenshotPreviewElement.append(colorLinkElement);
+                    colorLinkElement.click();
+                },
+                cursor: 'pointer'
+            },
+            ml: {
+                action: function (e, target) {
+                    target.remove();
+                    myThis.fabricCanvas.renderAll();
+                },
+                cursor: 'pointer'
+            }
+        });
+
+        fabric.Object.prototype.customiseCornerIcons({
+            settings: {
+                borderColor: 'black',
+                cornerSize: 24,
+                cornerShape: 'rect',
+                cornerPadding: 1
+            },
+            ml: {
+                icon: myThis.distPath + 'img/ic_delete_black_24px_background.svg'
+            },
+            mt: {
+                icon: myThis.distPath + 'img/ic_format_color_fill_black_24px_background.svg'
+            }
+        });
     }
 }
 
