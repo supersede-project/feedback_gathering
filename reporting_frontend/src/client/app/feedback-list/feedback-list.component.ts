ffb473b7f5460150ed0806ce9b68de48565cf945
rschaniel <ronnieschaniel@gmail.com>
2016-10-05 13:57:59 +0200

    csv export

--- reporting_frontend/src/client/app/feedback-list/feedback-list.component.ts
+++ reporting_frontend/src/client/app/feedback-list/feedback-list.component.ts
@@ -3,6 +3,8 @@ import {Feedback} from '../shared/models/feedbacks/feedback';
 import {Application} from '../shared/models/applications/application';
 import {FeedbackListService} from '../shared/services/feedback-list.service';
 import {ApplicationService} from '../shared/services/application.service';
+import {TextMechanism} from '../shared/models/mechanisms/text_mechanism';
+import {RatingMechanism} from '../shared/models/mechanisms/rating_mechanism';
 
 
 /**
@@ -49,6 +51,7 @@ export class FeedbackListComponent implements OnInit {
         for (var application of this.applications) {
           application.filterActive = false;
         }
+        this.populateConfigurationData();
       },
       error => this.errorMessage = <any>error
     );
@@ -107,6 +110,75 @@ export class FeedbackListComponent implements OnInit {
       this.filteredFeedbacks = this.feedbacks.filter(feedback => feedback.applicationId === application.id);
     } else {
       this.filteredFeedbacks = this.feedbacks;
+    }
+  }
+
+  /**
+   * combines repository with the orchestrator data
+   */
+  populateConfigurationData() {
+    for(var feedback of this.feedbacks) {
+      var application = this.applications.filter(application => application.id === feedback.applicationId)[0];
+      if(application) {
+        var configuration = application.configurations.filter(configuration => configuration.id === feedback.configurationId)[0];
+
+        for(var textFeedback of feedback.textFeedbacks) {
+          let textMechanism:TextMechanism = <TextMechanism>configuration.mechanisms.filter(mechanism => mechanism.id === textFeedback.mechanismId)[0];
+          textFeedback.mechanism = new TextMechanism(textMechanism.id, textMechanism.type, textMechanism.active, textMechanism.order, textMechanism.canBeActivated, textMechanism.parameters);
+        }
+        for(var ratingFeedback of feedback.ratingFeedbacks) {
+          let ratingMechanism:RatingMechanism = <RatingMechanism>configuration.mechanisms.filter(mechanism => mechanism.id === ratingFeedback.mechanismId)[0];
+          ratingFeedback.mechanism = new RatingMechanism(ratingMechanism.id, ratingMechanism.type, ratingMechanism.active, ratingMechanism.order, ratingMechanism.canBeActivated, ratingMechanism.parameters);
+        }
+      }
+    }
+  }
+
+  exportAsCSV():void {
+    var csvContent = "";
+    csvContent += "ID, Title, Date, Application, Feedbacks \n";
+
+    for (let feedback of this.filteredFeedbacks) {
+      csvContent += feedback.id + "," + feedback.title + "," + feedback.createdAt + "," + feedback.applicationId + ",";
+
+      for(let textFeedback of feedback.textFeedbacks) {
+        if(textFeedback.mechanism) {
+          csvContent += textFeedback.mechanism.getParameterValue('title') + ": " + textFeedback.text + ",";
+        } else {
+          csvContent += textFeedback.text + ",";
+        }
+      }
+
+      for(let ratingFeedback of feedback.ratingFeedbacks) {
+        if(ratingFeedback.mechanism) {
+          csvContent += ratingFeedback.mechanism.getParameterValue('title') + ": " + ratingFeedback.rating + "/" + ratingFeedback.mechanism.getParameterValue('maxRating') + ",";
+        } else {
+          csvContent += ratingFeedback.rating + ",";
+        }
+      }
+
+      // remove last comma
+      csvContent = csvContent.slice(0, -1);
+      csvContent += "\n";
+    }
+
+    var title = 'feedbacks';
+    var filename = title.replace(/ /g, '') + '.csv';
+    var blob = new Blob([csvContent], {"type": 'text/csv;charset=utf-8;'});
+    if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, filename);
+    } else {
+      var link = document.createElement("a");
+      if (link.download !== undefined)
+      {
+        var url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", filename);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
     }
   }
 }
