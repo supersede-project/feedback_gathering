254296be79f67469a4430995927d7df6af0f49a0
rschaniel <ronnieschaniel@gmail.com>
2016-10-25 08:36:05 +0200

    As a developer I can set a mandatory label style in the orchestrator for mechanisms that are mandatory

--- web_library/app/models/applications/application.js
+++ web_library/app/models/applications/application.js
@@ -1,4 +1,4 @@
-define(["require", "exports", '../configurations/general_configuration', '../configurations/configuration_factory', '../../js/config'], function (require, exports, general_configuration_1, configuration_factory_1, config_1) {
+define(["require", "exports", '../configurations/general_configuration', '../configurations/configuration_factory', '../../js/config', '../parameters/parameter_value_property_pair'], function (require, exports, general_configuration_1, configuration_factory_1, config_1, parameter_value_property_pair_1) {
     "use strict";
     var Application = (function () {
         function Application(id, name, state, generalConfiguration, configurations) {
@@ -24,9 +24,54 @@ define(["require", "exports", '../configurations/general_configuration', '../con
             return this.configurations.filter(function (configuration) { return configuration.type === config_1.configurationTypes.pull; });
         };
         Application.prototype.getContextForView = function () {
+            if (this.generalConfiguration.getParameterValue('reviewFontType') === 'bold') {
+                var reviewFontTypeCSSPair = new parameter_value_property_pair_1.ParameterValuePropertyPair('reviewFontType', 'font-weight');
+            }
+            else {
+                var reviewFontTypeCSSPair = new parameter_value_property_pair_1.ParameterValuePropertyPair('reviewFontType', 'font-style');
+            }
+            var reviewStyle = this.getCssStyle([
+                new parameter_value_property_pair_1.ParameterValuePropertyPair('reviewFontFamily', 'font-family'),
+                reviewFontTypeCSSPair
+            ]);
+            if (this.generalConfiguration.getParameterValue('mandatoryLabelStyle') === 'bold') {
+                var mandatoryLabelStyle = this.getCssStyle([
+                    new parameter_value_property_pair_1.ParameterValuePropertyPair('mandatoryLabelStyle', 'font-weight'),
+                ]);
+            }
+            else {
+                var mandatoryLabelStyle = this.getCssStyle([
+                    new parameter_value_property_pair_1.ParameterValuePropertyPair('mandatoryLabelStyle', 'font-style'),
+                ]);
+            }
             return {
-                reviewButtonPosition: this.generalConfiguration.getParameterValue('reviewButtonPosition')
+                reviewButtonPosition: this.generalConfiguration.getParameterValue('reviewButtonPosition'),
+                reviewStyle: reviewStyle,
+                mandatorySign: this.generalConfiguration.getParameterValue('mandatorySign'),
+                mandatoryLabelStyle: mandatoryLabelStyle
             };
+        };
+        Application.prototype.getCssStyle = function (parameterValuePropertyPair) {
+            var cssStyles = '';
+            for (var i = 0; i < parameterValuePropertyPair.length; i++) {
+                var parameterPropertyPair = parameterValuePropertyPair[i];
+                if (this.generalConfiguration.getParameterValue(parameterPropertyPair.parameter) !== null) {
+                    var unit = this.getCSSPropertyUnit(parameterPropertyPair.property);
+                    cssStyles += parameterPropertyPair.property + ': ' + this.generalConfiguration.getParameterValue(parameterPropertyPair.parameter) + unit + ';';
+                    if (i !== parameterValuePropertyPair.length - 1) {
+                        cssStyles += ' ';
+                    }
+                }
+            }
+            return cssStyles;
+        };
+        Application.prototype.getCSSPropertyUnit = function (property) {
+            if (property === 'font-size') {
+                return 'px';
+            }
+            else {
+                return '';
+            }
         };
         return Application;
     }());
