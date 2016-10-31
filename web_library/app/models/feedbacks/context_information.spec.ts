257614797c07a57dda01d44cf99d65ce4cb6a26e
rschaniel <ronnieschaniel@gmail.com>
2016-10-31 10:12:45 +0100

    clean up tests

--- web_library/app/models/feedbacks/context_information.spec.ts
+++ web_library/app/models/feedbacks/context_information.spec.ts
@@ -3,9 +3,8 @@ import {ContextInformation} from './context_information';
 
 describe('Context information', () => {
 
-    fit('should contain all relevant information when created', () => {
+    it('should contain all relevant information when created', () => {
         var contextInformation = ContextInformation.create();
-        console.log(contextInformation);
         expect(contextInformation.devicePixelRatio).not.toBeNull();
     });
 });
