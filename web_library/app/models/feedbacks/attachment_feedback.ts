484c130f6f3c1cd1bd6a4f790b51b5ac798a4b62
rschaniel <ronnieschaniel@gmail.com>
2016-10-31 11:05:53 +0100

    corrected attachment file extension

--- web_library/app/models/feedbacks/attachment_feedback.ts
+++ web_library/app/models/feedbacks/attachment_feedback.ts
@@ -1,13 +1,35 @@
 export class AttachmentFeedback {
     part:string;
     name:string;
-    extension:string;
+    fileExtension:string;
     mechanismId:number;
 
-    constructor(part:string, name:string, extension:string, mechanismId:number) {
+    constructor(part:string, name:string, fileExtension:string, mechanismId:number) {
         this.part = part;
         this.name = name;
-        this.extension = extension;
+        this.fileExtension = this.getFileExtensionFromContentType(fileExtension);
         this.mechanismId = mechanismId;
+    }
+
+    getFileExtensionFromContentType(fileContentType:string):string {
+        if(!fileContentType) {
+            return "";
+        } else if(fileContentType === 'application/pdf') {
+            return "pdf";
+        } else if(fileContentType === 'application/msword') {
+            return "doc";
+        } else if(fileContentType === 'image/jpeg') {
+            return "jpg";
+        } else if(fileContentType === 'image/png') {
+            return "png";
+        } else if(fileContentType === 'text/plain') {
+            return "txt";
+        } else {
+            if(fileContentType.split('/').length > 1) {
+                return fileContentType.split('/')[1];
+            } else {
+                return "";
+            }
+        }
     }
 }
