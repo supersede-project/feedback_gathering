9ac4817e5a087f6f5d05dd594ec53bafe13e934e
rschaniel <ronnieschaniel@gmail.com>
2016-10-31 10:14:31 +0100

    removed e2e test

--- web_library/app/e2e/feedback_plugin.e2e.js
+++ web_library/app/e2e/feedback_plugin.e2e.js
@@ -12,16 +12,5 @@ describe('JQuery Feedback Plugin', function () {
         var feedbackDialog = browser.element(by.css('section#feedbackContainer'));
         expect(feedbackDialog.isPresent()).toEqual(true);
     });
-    xit('should validate text input', function () {
-        var feedbackEntryLink = browser.element(by.css('a#feedbackEntryPoint'));
-        var dialogForward = browser.element(by.css('.feedback-dialog-forward'));
-        var textTypeTextarea = browser.element(by.css('textarea.textTypeText'));
-        feedbackEntryLink.click();
-        browser.sleep(2000);
-        textTypeTextarea.clear();
-        dialogForward.click();
-        console.log(feedbackPage2);
-        expect(feedbackPage2.isPresent()).toEqual(false);
-    });
 });
 //# sourceMappingURL=feedback_plugin.e2e.js.map
