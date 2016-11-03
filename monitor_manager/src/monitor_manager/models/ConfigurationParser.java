be1bab5d8daf7c876564e52ea23fd19cd31debcf
jmotger <joaquim.motger@est.fib.upc.edu>
2016-11-03 15:54:52 +0100

    monitor manager parser simplifier

--- monitor_manager/src/monitor_manager/models/ConfigurationParser.java
+++ monitor_manager/src/monitor_manager/models/ConfigurationParser.java
@@ -5,41 +5,15 @@ import com.google.gson.JsonObject;
 public class ConfigurationParser {
 
 	public JsonObject getTwitterConfiguration(JsonObject json) {
-		JsonObject socialNetworks = new JsonObject();
-		JsonObject params = new JsonObject();
-		addMainConfigurationParams(json,params);
-		if (json.get("keywordEpxression") != null) params.addProperty("keywordExpression", json.get("keywordExpression").getAsString());
-		if (json.get("accounts") != null) params.add("accounts", json.get("accounts"));
-		
-		socialNetworks.add("SocialNetworksMonitoringConfProf", params);
-		return socialNetworks;
+		return json.getAsJsonObject("SocialNetworksMonitorConfiguration");
 	}
 	
 	public JsonObject getGooglePlayConfiguration(JsonObject json) {
-		JsonObject googlePlay = new JsonObject();
-		JsonObject params = new JsonObject();
-		addMainConfigurationParams(json,params);
-		params.addProperty("packageName", json.get("packageName").getAsString());
-		
-		googlePlay.add("GooglePlayConfProf", params);
-		return googlePlay;
+		return json.getAsJsonObject("GooglePlayConfiguration");
 	}
 	
 	public JsonObject getAppStoreConfiguration(JsonObject json) {
-		JsonObject appStore = new JsonObject();
-		JsonObject params = new JsonObject();
-		addMainConfigurationParams(json,params);
-		params.addProperty("appId", json.get("appId").getAsString());
-		
-		appStore.add("AppStoreConfProf", params);
-		return appStore;
-	}
-	
-	private void addMainConfigurationParams(JsonObject json, JsonObject params) {
-		params.addProperty("toolName", json.get("tool").getAsString());
-		params.addProperty("timeSlot", json.get("timeSlot").getAsString());
-		params.addProperty("kafkaEndpoint", json.get("kafkaEndpoint").getAsString());
-		params.addProperty("kafkaTopic", json.get("kafkaTopic").getAsString());
+		return json.getAsJsonObject("AppStoreConfiguration");
 	}
 	
 }
