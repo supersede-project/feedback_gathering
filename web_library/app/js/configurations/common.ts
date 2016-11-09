7e25dda217e1b4b732fae2c850ee4f1ff264a2b9
rschaniel <ronnieschaniel@gmail.com>
2016-11-09 10:27:11 +0100

    created all configurations

---
+++ web_library/app/js/configurations/common.ts
@@ -0,0 +1,26 @@
+export const defaultSuccessMessage = 'Your feedback was successfully sent';
+export const dialogOptions = {
+    autoOpen: false,
+    height: 'auto',
+    width: 'auto',
+    minWidth: 500,
+    modal: true,
+    title: 'Feedback',
+    buttons: {},
+    resizable: false
+};
+export const mechanismTypes = {
+    textType: 'TEXT_TYPE',
+    ratingType: 'RATING_TYPE',
+    screenshotType: 'SCREENSHOT_TYPE',
+    audioType: 'AUDIO_TYPE',
+    categoryType: 'CATEGORY_TYPE',
+    attachmentType: 'ATTACHMENT_TYPE'
+};
+export const configurationTypes = {
+    push: 'PUSH',
+    pull: 'PULL'
+};
+export const cookieNames = {
+    lastTriggered: 'lastTriggered'
+};
---
+++ web_library/app/js/configurations/default.ts
@@ -0,0 +1,7 @@
+export const apiEndpointOrchestrator = 'http://ec2-54-175-37-30.compute-1.amazonaws.com/';
+export const applicationPath = 'orchestrator/feedback/{lang}/applications/';
+export const apiEndpointRepository = 'http://ec2-54-175-37-30.compute-1.amazonaws.com/';
+export const feedbackPath = "feedback_repository/{lang}/feedbacks";
+export const applicationId = 4;
+export const applicationName = 'energiesparkonto.de';
+export const feedbackObjectTitle = 'Feedback';
