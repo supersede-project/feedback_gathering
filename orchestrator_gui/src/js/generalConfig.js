26d49d61da8e4234e69600025694668318cfb67f
Lorenz <lorenz.zmoos@gmail.com>
2016-12-13 15:15:26 +0100

    separated general configurations

---
+++ orchestrator_gui/src/js/generalConfig.js
@@ -0,0 +1,28 @@
+var labelFontType = "Helvetica";
+var labelFontColor = "#000";
+var labelFontSize = "12";
+var submitText = "Success";
+
+$(document).ready(function () {
+    /* preview update on generalField setting font and fontcolor */
+    $("select#setFont").change(setFontFamily);
+
+    function setFontFamily() {
+        labelFontType = $("#setFont").val();
+        $( "#preview #viewDiv" ).removeClass().addClass(labelFontType);
+    }
+
+    $("#setFontColor").change(function () {
+        labelFontColor = $("#setFontColor").val();
+        $("#preview #viewDiv").css("color", labelFontColor);
+    });
+
+    $("#fontSize").change(function() {
+        labelFontSize = $("#fontSize").val();
+        $("#preview #viewDiv").css("font-size", labelFontSize + "px");
+    });
+
+    $("#submitText").change(function() {
+        submitText = $("#submitText").val();
+    });
+});
