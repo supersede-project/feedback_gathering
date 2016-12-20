d5a4fb77b2acda10616d01ed9d48bf388bef047f
rschaniel <ronnieschaniel@gmail.com>
2016-12-20 17:33:07 +0100

    added application status toggle

--- reporting_frontend/src/client/app/settings/settings.component.ts
+++ reporting_frontend/src/client/app/settings/settings.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {NotificationSettingService} from '../shared/services/notification-setting.service';
 import {NotificationSetting} from '../shared/models/settings/notification_setting';
+import {ApplicationService} from '../shared/services/application.service';
+import {Application} from '../shared/models/applications/application';
 
 
 @Component({
@@ -11,13 +13,17 @@ import {NotificationSetting} from '../shared/models/settings/notification_settin
 })
 export class SettingsComponent implements OnInit {
   notificationSettings:NotificationSetting[] = [];
+  application:Application;
+  // TODO replace
+  applicationId:number = 8;
 
-  constructor(private notificationSettingService:NotificationSettingService) {
+  constructor(private notificationSettingService:NotificationSettingService, private applicationService:ApplicationService) {
 
   }
 
   ngOnInit() {
     this.getNotificationSettings();
+    this.getApplication(this.applicationId);
   }
 
   getNotificationSettings() {
@@ -30,6 +36,28 @@ export class SettingsComponent implements OnInit {
           console.log(error);
         }
       );
+  }
+
+  getApplication(id:number) {
+    this.applicationService.find(id).subscribe(
+      application => {
+        this.application = application;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  toggleApplicationState(id:number) {
+    this.applicationService.toggleState(id).subscribe(
+      application => {
+        this.application = application;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
   update(id:number, email:string, applicationId:number, active:boolean) {
--- reporting_frontend/src/client/app/shared/services/application.service.ts
+++ reporting_frontend/src/client/app/shared/services/application.service.ts
@@ -1,10 +1,10 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {Http, Response, Headers} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import {Application} from '../models/applications/application';
-import {ORCHESTRATOR_HOST} from './config';
+import {ORCHESTRATOR_HOST, RAILS_ORCHESTRATOR_HOST} from './config';
 
 declare function camelizer(obj: any): any;
 
@@ -24,6 +24,16 @@ export class ApplicationService {
     return this.http.get(ORCHESTRATOR_HOST + 'en/applications/' + id)
       .map((res: Response) => camelizer(res.json()))
       .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  toggleState(id:number):Observable<Application> {
+    var headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Authorization', localStorage.getItem('auth_token'));
+
+    return this.http.put(RAILS_ORCHESTRATOR_HOST + 'en/applications/' + id + '/toggle_state', JSON.stringify(''), { headers: headers })
+      .map((res: Response) => camelizer(res.json()))
+      .catch((error:any) => Observable.throw(error || 'Server error'));
   }
 }
 
