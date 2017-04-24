dd9026716b8d176f415290eab440f75f3c81700b
Panagiotis Michalopoulos <Panagiotis Michalopoulos>
2017-04-24 12:44:32 +0200

    the monitor for Siemens JSON formatted log files - last updates

--- monitors/JsonLogs/src/main/java/monitoring/tools/SendJSONs.java
+++ monitors/JsonLogs/src/main/java/monitoring/tools/SendJSONs.java
@@ -1,14 +1,11 @@
 package monitoring.tools;
 
-
-import java.io.File;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Pattern;
+
 
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
@@ -32,58 +29,41 @@ public class SendJSONs {
 	}
 
 
-	public void generateData(String filename, String kafkaEndPoint, String kafkaTopic) throws Exception {
+	public void generateData(String json, String kafkaTopic) throws Exception {
 		
 		//logger.debug("Initialising kafka producer...");
 		//kafka.initProducer(kafkaEndPoint);
 		logger.debug("Initialising proxy...");
-		kafka.initProxy(kafkaEndPoint);
+		kafka.initProxy();
 		
 		String timestamp;
 		
-		List<JsonLogsMonitoringData> data = new ArrayList<>();		
-		
-		String input = "";
+		List<JsonLogsMonitoringData> data = new ArrayList<>();
 		
 		try{
-			Scanner scanner = new Scanner(new File(filename));
-			scanner.useDelimiter(Pattern.compile("}"));
-
-			while(scanner.hasNext()) {
-				input = scanner.next().trim();
-				if (!input.equals("")){
-					input+="\n}";
-				}
-				else{
-					continue;
-				}				
+			
+			JsonLogsMonitoringData params = new JsonLogsMonitoringData();
+			
+			JSONObject jsonParams = new JSONObject(json);
 				
-				JsonLogsMonitoringData params = new JsonLogsMonitoringData();
-				
-				JSONObject jsonParams = new JSONObject(input);
-				
-				Iterator<?> keys = jsonParams.keys();
-				while( keys.hasNext() ) {
-				    String key = (String)keys.next();
-				    if (key.equals("timeMillis")) params.setTimeMillis(jsonParams.getBigInteger(key));
-				    else if (key.equals("thread")) params.setThread(jsonParams.getString(key).replaceAll("\"", ""));
-				    else if (key.equals("level")) params.setLevel(jsonParams.getString(key).replaceAll("\"", ""));
-				    else if (key.equals("loggerName")) params.setLoggerName(jsonParams.getString(key).replaceAll("\"", ""));
-				    else if (key.equals("message")) params.setMessage(jsonParams.getString(key).replaceAll("\"", ""));
-				    else if (key.equals("endOfBatch")) params.setEndOfBatch(jsonParams.getBoolean(key));
-				    else if (key.equals("loggerFqcn")) params.setLoggerFqcn(jsonParams.getString(key).replaceAll("\"", ""));
-				    else if (key.equals("threadId")) params.setThreadId(jsonParams.getInt(key));
-				    else if (key.equals("threadPriority")) params.setThreadPriority(jsonParams.getInt(key));
-				}
-				
-				
-				data.add(params);
-				JSONObject json_obj = params.toJsonObject();
-				logger.debug("Sent data: " + json_obj.toString());
-				
-				
+			Iterator<?> keys = jsonParams.keys();
+			while( keys.hasNext() ) {
+				String key = (String)keys.next();
+				if (key.equals("timeMillis")) params.setTimeMillis(jsonParams.getBigInteger(key));
+				else if (key.equals("thread")) params.setThread(jsonParams.getString(key).replaceAll("\"", ""));
+				else if (key.equals("level")) params.setLevel(jsonParams.getString(key).replaceAll("\"", ""));
+				else if (key.equals("loggerName")) params.setLoggerName(jsonParams.getString(key).replaceAll("\"", ""));
+				else if (key.equals("message")) params.setMessage(jsonParams.getString(key).replaceAll("\"", ""));
+				else if (key.equals("endOfBatch")) params.setEndOfBatch(jsonParams.getBoolean(key));
+				else if (key.equals("loggerFqcn")) params.setLoggerFqcn(jsonParams.getString(key).replaceAll("\"", ""));
+				else if (key.equals("threadId")) params.setThreadId(jsonParams.getInt(key));
+				else if (key.equals("threadPriority")) params.setThreadPriority(jsonParams.getInt(key));
 			}
-			scanner.close();
+				
+			data.add(params);
+			JSONObject json_obj = params.toJsonObject();
+			logger.debug("Sent data: " + json_obj.toString());
+				
 			
 			timestamp = new Timestamp((new Date()).getTime()).toString();
 			//kafka.generateResponseKafka(data, timestamp, id, configurationId, kafkaTopic, "JSONFiles");
