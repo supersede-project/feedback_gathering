a0a03dd817850006a322786683ba4d99d343f94b
rschaniel <ronnieschaniel@gmail.com>
2017-08-08 15:08:51 +0200

    improved audio mechanism

--- web_library/app/views/audio/audio_view.ts
+++ web_library/app/views/audio/audio_view.ts
@@ -10,7 +10,13 @@ export class AudioView implements MechanismView {
     stopButton:JQuery;
     deleteButton:JQuery;
     replayButton:JQuery;
+    stopReplayButton:JQuery;
     audioElement:JQuery;
+    recordStart:Date;
+    recordInterval:any;
+    replayStart:Date;
+    replayInterval:any;
+    recordTime:number;
 
     constructor(audioMechanism:AudioMechanism, container:JQuery, distPath:string) {
         this.audioMechanism = audioMechanism;
@@ -25,7 +31,13 @@ export class AudioView implements MechanismView {
         this.stopButton = this.container.find('a.stop');
         this.deleteButton = this.container.find('a.delete');
         this.replayButton = this.container.find('a.replay');
+        this.stopReplayButton = this.container.find('a.stop-replay');
         this.audioElement = this.container.find('audio.audio');
+
+        this.stopButton.hide();
+        this.deleteButton.hide();
+        this.replayButton.hide();
+        this.stopReplayButton.hide();
     }
 
     initEvents() {
@@ -33,11 +45,19 @@ export class AudioView implements MechanismView {
         this.recordButton.on('click', function() {
             Fr.voice.record(false, function () {
                 myThis.recordButton.addClass("disabled");
-
                 var activeSrc = myThis.recordButton.find('img').data('active-src');
                 myThis.recordButton.find('img').attr('src', activeSrc);
                 myThis.container.find(".one").removeClass("disabled");
             });
+            myThis.recordStart = new Date();
+            myThis.recordInterval = setInterval(function() {
+                let seconds:number = (new Date().getTime() - myThis.recordStart.getTime()) / 1000;
+                seconds = Math.abs(seconds);
+                myThis.recordTime = seconds;
+                myThis.container.find('.audio-time').empty().text(myThis.toMMSS(seconds));
+            }, 1000);
+
+            myThis.stopButton.show();
             myThis.container.addClass('dirty');
         });
 
@@ -46,6 +66,11 @@ export class AudioView implements MechanismView {
             Fr.voice.export(function (url) {
                 myThis.audioElement.attr("src", url);
             }, "URL");
+            myThis.replayButton.show();
+            myThis.deleteButton.show();
+            myThis.stopButton.hide();
+
+            clearInterval(myThis.recordInterval);
         });
 
         this.deleteButton.on('click', function() {
@@ -59,6 +84,28 @@ export class AudioView implements MechanismView {
                 myThis.audioElement.attr("src", url);
                 myThis.audioElement[0].play();
             }, "URL");
+
+            myThis.stopReplayButton.show();
+
+            myThis.replayStart = new Date();
+            myThis.replayInterval = setInterval(function() {
+                let seconds:number = (new Date().getTime() - myThis.replayStart.getTime()) / 1000;
+                seconds = Math.abs(seconds);
+
+                // make sure the audio element has started to replay before checking whether it's paused
+                if(seconds > 1 && myThis.audioElement[0].paused) {
+                    clearInterval(myThis.replayInterval);
+                    myThis.stopReplayButton.hide();
+                }
+                if(seconds <= myThis.recordTime) {
+                    myThis.container.find('.replay-time').empty().text(myThis.toMMSS(seconds) + " / ");
+                }
+            }, 1000);
+        });
+
+        this.stopReplayButton.on('click', function() {
+            myThis.audioElement[0].stop();
+            clearInterval(myThis.replayInterval);
         });
     }
 
@@ -75,7 +122,27 @@ export class AudioView implements MechanismView {
     }
 
     reset() {
+        clearInterval(this.recordInterval);
+        clearInterval(this.replayInterval);
+        this.stopButton.hide();
+        this.deleteButton.hide();
+        this.replayButton.hide();
+        this.stopReplayButton.hide();
+        this.container.find('.replay-time').empty();
+        this.container.find('.audio-time').empty();
+
         this.container.removeClass('dirty');
+    }
+
+    toMMSS(totalSeconds:number) {
+        let hours   = Math.floor(totalSeconds / 3600);
+        let minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
+        let seconds = totalSeconds - (hours * 3600) - (minutes * 60);
+
+        if (hours   < 10) {hours   = "0"+hours;}
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+        return minutes + ':' + seconds.substring(0, 2);
     }
 }
 
