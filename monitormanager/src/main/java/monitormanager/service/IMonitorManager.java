ce853380a7993f32434f6db8409f463d43c2e15f
jmotger <joaquim.motger@est.fib.upc.edu>
2017-10-18 08:59:46 +0200

    restore JSON mapping for monitor manager - fixed bug

--- monitormanager/src/main/java/monitormanager/service/IMonitorManager.java
+++ monitormanager/src/main/java/monitormanager/service/IMonitorManager.java
@@ -4,9 +4,9 @@ import com.google.gson.JsonObject;
 
 public interface IMonitorManager {
 	
-	JsonObject addConfiguration(String monitorName, JsonObject jsonObj) throws Exception;
+	JsonObject addConfiguration(String monitorName, String input) throws Exception;
 	
-	JsonObject updateConfiguration(String monitorName, int confId, JsonObject jsonObj) throws Exception;
+	JsonObject updateConfiguration(String monitorName, int confId, String input) throws Exception;
 	
 	void deleteConfiguration(String monitorName, int confId) throws Exception;
 
