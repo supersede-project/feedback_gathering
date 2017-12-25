61b1c403e20d133b242cbfe8011838b6facef21d
rschaniel <ronnieschaniel@gmail.com>
2017-12-25 13:46:32 +0100

    atos changes

--- web_library/app/js/config.ts
+++ web_library/app/js/config.ts
@@ -26,10 +26,10 @@ export const configurationTypes = {
 export const cookieNames = {
     lastTriggered: 'lastTriggered'
 };
-export const apiEndpointOrchestrator = 'https://supersede-develop.atosresearch.eu:8443/';
+export const apiEndpointOrchestrator = 'http://supersede.es.atos.net:8081/';
 export const applicationPath = 'orchestrator/feedback/{lang}/applications/';
-export const apiEndpointRepository = 'https://supersede-develop.atosresearch.eu:8443/';
+export const apiEndpointRepository = 'http://supersede.es.atos.net:8081/';
 export const feedbackPath = "feedback_repository/{lang}/applications/{applicationId}/feedbacks";
-export const applicationId = 20;
-export const applicationName = 'Senercon';
-export const feedbackObjectTitle = 'Feedback Senercon';
+export const applicationId = 14;
+export const applicationName = 'ATOS';
+export const feedbackObjectTitle = 'Feedback ATOS';
