2072f303e4f8c26312598542c11a6608eb76268b
KuersatAydinli <kuersat.aydinli@uzh.ch>
2018-01-29 17:22:56 +0100

    upload/retrieve profile picture including size checking

--- repository/src/main/java/ch/fhnw/cere/repository/controllers/UploadController.java
+++ repository/src/main/java/ch/fhnw/cere/repository/controllers/UploadController.java
@@ -1,12 +1,20 @@
 package ch.fhnw.cere.repository.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
+import javax.print.attribute.standard.Media;
+import javax.servlet.ServletContext;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -22,21 +30,27 @@ import java.util.StringJoiner;
 @RestController
 @RequestMapping(value = "${supersede.base_path.feedback}/{language}/applications/{applicationId}/feedbacks")
 public class UploadController {
-    private static String UPLOADED_FOLDER = "F://supersede_profilepics//";
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(FeedbackController.class);
+
+    @Value("${supersede.profile_pic_upload_directory}")
+    protected String UPLOADED_FOLDER;
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String singleFileUpload(@RequestParam("file") MultipartFile file){
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   @RequestHeader(value = "api_user_id") long api_user_id){
         try {
-            // Get the file and save it somewhere
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+//            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Path path = Paths.get(UPLOADED_FOLDER + "/profile_pic_api_user_"+api_user_id + "."
+                    + FilenameUtils.getExtension(file.getOriginalFilename()));
             Files.write(path, bytes);
 
             return "successfully uploaded file";
 
         } catch (IOException e) {
             e.printStackTrace();
-            return "did not work";
+            return "file upload did not work";
         }
     }
 
@@ -64,14 +78,34 @@ public class UploadController {
     }
 
     @RequestMapping(value = "/uploadImage", method = RequestMethod.POST)
-    public String handleFormUpload(@RequestParam("file") MultipartFile file) throws IOException{
+    public String handleFormUpload(@RequestParam("file") MultipartFile file,
+                                   @RequestHeader(value = "api_user_id") long api_user_id) throws IOException{
         if (!file.isEmpty()) {
             BufferedImage src = ImageIO.read(new ByteArrayInputStream(file.getBytes()));
-            File destination = new File(UPLOADED_FOLDER + "//test_pic_1.png"); // something like C:/Users/tom/Documents/nameBasedOnSomeId.png
-            ImageIO.write(src, "png", destination);
-            //Save the id you have used to create the file name in the DB. You can retrieve the image in future with the ID.
-            return "Image successfully uploaded";
+            LOGGER.info("Upload Image Size: " + "("+src.getWidth()+" , "+src.getHeight()+")");
+            if(src.getWidth() < 40 || src.getHeight() < 40){
+                return "The uploaded image size is too small. Please select an image with at least " +
+                        "40 x 40 Pixels";
+            } else {
+                String format = FilenameUtils.getExtension(file.getOriginalFilename());
+                File destination = new File(UPLOADED_FOLDER + "//profile_pic_api_user_"+
+                        api_user_id + "." + format); // something like C:/Users/tom/Documents/nameBasedOnSomeId.png
+                ImageIO.write(src, format, destination);
+                return "Image successfully uploaded";
+            }
         }
         return "Image is empty";
+    }
+
+
+    @RequestMapping(value = "/getImage", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getImage(@RequestHeader(value = "api_user_id") long api_user_id) throws IOException {
+        Path path = Paths.get(UPLOADED_FOLDER + "/profile_pic_api_user_"+api_user_id + ".png");
+        byte[] bytes = Files.readAllBytes(path);
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        return new ResponseEntity<byte[]> (bytes, headers, HttpStatus.CREATED);
     }
 }
