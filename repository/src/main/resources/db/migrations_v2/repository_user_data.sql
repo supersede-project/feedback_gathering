082f9898339f4ba9d01492b142364ec1f42a1a3d
rschaniel <ronnieschaniel@gmail.com>
2018-01-30 15:39:45 +0100

    *table structure v2 *api user

---
+++ repository/src/main/resources/db/migrations_v2/repository_user_data.sql
@@ -0,0 +1,37 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50710
+ Source Host           : localhost
+ Source Database       : supersede_repository_spring
+
+ Target Server Type    : MySQL
+ Target Server Version : 50710
+ File Encoding         : utf-8
+
+ Date: 01/30/2018 15:37:22 PM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+-- ----------------------------
+--  Records of `api_user`
+-- ----------------------------
+BEGIN;
+INSERT INTO `api_user` VALUES ('1', 'admin', '$2a$10$HM01Z2Vfhw5s4LG2Svze1.IpC7Vn0vkU1or.4h9WaSbTRwwOCOEAy');
+COMMIT;
+
+
+-- ----------------------------
+--  Records of `api_user_api_user_role`
+-- ----------------------------
+BEGIN;
+INSERT INTO `api_user_api_user_role` VALUES ('1', '1', '1');
+COMMIT;
+
+
+SET FOREIGN_KEY_CHECKS = 1;
