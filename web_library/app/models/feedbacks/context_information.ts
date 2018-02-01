e9cd2e3a15ec11682f8b648e0bf384a3c542e07b
rschaniel <ronnieschaniel@gmail.com>
2018-02-01 11:01:00 +0100

    local time now in unix epoch

--- web_library/app/models/feedbacks/context_information.ts
+++ web_library/app/models/feedbacks/context_information.ts
@@ -22,21 +22,18 @@ export class ContextInformation {
     }
 
     static create(metaData) {
-        var resolution = window.screen.availHeight + 'x' + window.screen.availWidth;
-        var userAgent = navigator.userAgent;
+        let resolution = window.screen.availHeight + 'x' + window.screen.availWidth;
+        let userAgent = navigator.userAgent;
 
-        var d = new Date();
-        var localTime = new Date(d.getTime() - d.getTimezoneOffset() * 60000).toISOString();
-        localTime = localTime.replace('T', ' ');
-        localTime = localTime.replace('Z', ' ');
-        localTime = localTime.slice(0, -3);
-        var timeZone = d.toString().split("GMT")[1].split(" (")[0];
+        let d = new Date();
+        let localTime = d.getTime() - d.getTimezoneOffset();
+        let timeZone = d.toString().split("GMT")[1].split(" (")[0];
 
-        var devicePixelRatio = ContextInformation.getDevicePixelRatio();
-        var country = null;
-        var region = null;
-        var url = window.location.href;
-        return new ContextInformation(resolution, userAgent, localTime, timeZone, devicePixelRatio, country, region, url, JSON.stringify(metaData));
+        let devicePixelRatio = ContextInformation.getDevicePixelRatio();
+        let country = null;
+        let region = null;
+        let url = window.location.href;
+        return new ContextInformation(resolution, userAgent, "" + localTime, timeZone, devicePixelRatio, country, region, url, JSON.stringify(metaData));
     }
 
     private static getDevicePixelRatio():number {
