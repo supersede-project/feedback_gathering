85c4c139ff764404858fe36af2d7a74a30e577c4
rschaniel <ronnieschaniel@gmail.com>
2018-06-26 17:38:28 +0200

    added correct base class for controller

--- repository/src/main/java/ch/fhnw/cere/repository/controllers/ReportingController.java
+++ repository/src/main/java/ch/fhnw/cere/repository/controllers/ReportingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "${supersede.base_path.feedback}/{language}/applications/{applicationId}/reports")
-public class ReportingController {
+public class ReportingController extends BaseController {
 
     @Autowired
     private ReportService reportService;
